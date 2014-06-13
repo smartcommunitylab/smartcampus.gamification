c2e9a44e37ab8ec47d875142eb783b763d8d1162
Mirko Martignon <mirko.martignon@hotmail.it>
2014-06-13 13:45:43 +0200

    Still working

--- GamificationClient/js/app.js
+++ GamificationClient/js/app.js
@@ -1,1 +1,16 @@
 var app = angular.module('gamificationEngine', ['ui.bootstrap', 'localization']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.when('/login', {
+        templateUrl: 'templates/login.html',
+        controller: 'TmpCtrl'
+    });
+    $routeProvider.otherwise({
+        redirectTo: '/login'
+    });
+
+});
+
+function TmpCtrl($scope) {
+
+}

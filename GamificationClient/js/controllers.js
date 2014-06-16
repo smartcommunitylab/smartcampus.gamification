eb37b786ef3cd2e7ef87a817631cbfbde568ff44
Mirko Martignon <mirko.martignon@hotmail.it>
2014-06-16 13:20:44 +0200

    Data are now loaded from a json object defined in javascript

--- GamificationClient/js/controllers.js
+++ GamificationClient/js/controllers.js
@@ -3,6 +3,7 @@ function LoginCtrl($scope) {
 }
 
 function HomeCtrl($scope, $modal) {
+  $scope.games = games;
   $scope.openGameModal = function () {
 
     var modalInstance = $modal.open({
@@ -27,3 +28,26 @@ function GameModalInstanceCtrl($scope, $modalInstance) {
     $modalInstance.dismiss('cancel');
   };
 }
+
+var games = [
+  {
+    'name': 'Game 1. Lorem ipsum dolor sit amet.',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla justo sit amet aliquam congue. Ut malesuada ligula enim, ut ultricies tortor tempor bibendum. Donec id tempor eros. Aenean bibendum leo ut ipsum sollicitudin, et faucibus nulla sollicitudin. Pellentesque suscipit egestas varius. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla justo sit amet aliquam congue. Ut malesuada ligula enim, ut ultricies tortor tempor bibendum. Donec id tempor eros. Aenean bibendum leo ut ipsum sollicitudin, et faucibus nulla sollicitudin. Pellentesque suscipit egestas varius. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla justo sit amet aliquam congue. Ut malesuada ligula enim, ut ultricies tortor tempor bibendum. Donec id tempor eros. Aenean bibendum leo ut ipsum sollicitudin, et faucibus nulla sollicitudin. Pellentesque suscipit egestas varius.'
+    },
+  {
+    'name': 'Game 2',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla justo sit amet aliquam congue. Ut malesuada ligula enim, ut ultricies tortor tempor bibendum.'
+    },
+  {
+    'name': 'Game 3',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla justo sit amet aliquam congue. Ut malesuada ligula enim, ut ultricies tortor tempor bibendum. Donec id tempor eros. Aenean bibendum leo ut ipsum sollicitudin, et faucibus nulla sollicitudin. Pellentesque suscipit egestas varius.'
+    },
+  {
+    'name': 'Game 4',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    },
+  {
+    'name': 'Game 5',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur fringilla justo sit amet aliquam congue. Ut malesuada ligula enim, ut ultricies tortor tempor bibendum. Donec id tempor eros. Aenean bibendum leo ut ipsum sollicitudin, et faucibus nulla sollicitudin. Pellentesque suscipit egestas varius.'
+    }
+];

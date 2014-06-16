634489258ba06e68fea38d4a3c00969ec0470561
Mirko Martignon <mirko.martignon@hotmail.it>
2014-06-16 10:49:36 +0200

    Working on modals

--- GamificationClient/js/controllers.js
+++ GamificationClient/js/controllers.js
@@ -2,6 +2,27 @@ function LoginCtrl($scope) {
 
 }
 
-function HomeCtrl($scope) {
+function HomeCtrl($scope, $modal) {
+  $scope.openAddGameModal = function () {
 
+    var modalInstance = $modal.open({
+      templateUrl: 'templates/addGameModal.html',
+      controller: AddGameModalInstanceCtrl,
+    });
+
+    modalInstance.result.then(function (game) {
+      /* TODO: Add new game! */
+      $scope.game = game;
+    });
+  };
+}
+
+function AddGameModalInstanceCtrl($scope, $modalInstance) {
+  $scope.ok = function () {
+    $modalInstance.close($scope.game);
+  };
+
+  $scope.cancel = function () {
+    $modalInstance.dismiss('cancel');
+  };
 }

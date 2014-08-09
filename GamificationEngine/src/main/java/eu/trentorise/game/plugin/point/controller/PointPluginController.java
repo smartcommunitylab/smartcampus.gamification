8343dd4c67af66d3947ac8ad0518b86ad3af9f7c
Luca Piras <luca.piras@trentorise.eu>
2014-08-09 18:23:40 +0200

    Gamification Engine:
    - service implementation and refactoring:
    
    	- refactoring of the web app for the path
    	- refactoring of the CRUD related to the CustomizedPlugin services (createCustomizedPlugin)
    	- created the related tests for the services implemented
    	- refactoring of the infrastucture for rest services
    	- fixed a bug related to the infrastucture for rest services (regarding exception handling)

--- GamificationEngine/src/main/java/eu/trentorise/game/plugin/point/controller/PointPluginController.java
+++ GamificationEngine/src/main/java/eu/trentorise/game/plugin/point/controller/PointPluginController.java
@@ -14,15 +14,12 @@ import java.util.Map;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.util.UriComponentsBuilder;
 
 

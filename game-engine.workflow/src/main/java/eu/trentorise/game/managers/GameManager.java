4399605e314811970cce58dd85a5e8be7c6e098e
mirkoperillo <mirko.perillo@gmail.com>
2015-02-11 11:02:56 +0100

    clean code

--- game-engine.workflow/src/main/java/eu/trentorise/game/managers/GameManager.java
+++ game-engine.workflow/src/main/java/eu/trentorise/game/managers/GameManager.java
@@ -2,11 +2,8 @@ package eu.trentorise.game.managers;
 
 import java.io.File;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,7 +14,6 @@ import org.springframework.stereotype.Component;
 import eu.trentorise.game.core.AppContextProvider;
 import eu.trentorise.game.core.GameContext;
 import eu.trentorise.game.core.GameTask;
-import eu.trentorise.game.core.TaskSchedule;
 import eu.trentorise.game.model.ClasspathRule;
 import eu.trentorise.game.model.DBRule;
 import eu.trentorise.game.model.FSRule;
@@ -28,7 +24,6 @@ import eu.trentorise.game.repo.GameRepo;
 import eu.trentorise.game.repo.RuleRepo;
 import eu.trentorise.game.services.GameService;
 import eu.trentorise.game.services.TaskService;
-import eu.trentorise.game.task.ClassificationTask;
 
 @Component
 public class GameManager implements GameService {
@@ -50,159 +45,6 @@ public class GameManager implements GameService {
 
 	@Autowired
 	RuleRepo ruleRepo;
-
-	// @PostConstruct
-	@SuppressWarnings("unused")
-	private void initRepo() {
-		repo = new HashMap<String, String>();
-		repo.put("save_itinerary", "game1");
-		repo.put("classification", "game1");
-
-		String gameId = "game1";
-		// init game1 for dev purpose
-		if (loadGameDefinitionById(gameId) == null) {
-			Game game = new Game();
-			game.setId(gameId);
-			game.setName("rovereto explorer");
-
-			game.setActions(new HashSet<String>());
-			game.getActions().add("save_itinerary");
-			game.getActions().add("classification");
-
-			game.setRules(new HashSet<String>());
-
-			game.getRules().add(
-					"classpath://rules/" + gameId
-							+ "/finalClassificationBadges.drl");
-			game.getRules().add(
-					"classpath://rules/" + gameId + "/greenBadges.drl");
-			game.getRules().add(
-					"classpath://rules/" + gameId + "/greenPoints.drl");
-			game.getRules().add(
-					"classpath://rules/" + gameId + "/healthBadges.drl");
-			game.getRules().add(
-					"classpath://rules/" + gameId + "/healthPoints.drl");
-			game.getRules().add(
-					"classpath://rules/" + gameId + "/initState.drl");
-			game.getRules()
-					.add("classpath://rules/" + gameId + "/prBadges.drl");
-			game.getRules()
-					.add("classpath://rules/" + gameId + "/prPoints.drl");
-			game.getRules().add(
-					"classpath://rules/" + gameId + "/specialBadges.drl");
-			game.getRules().add(
-					"classpath://rules/" + gameId
-							+ "/weekClassificationBadges.drl");
-
-			game.setTasks(new HashSet<GameTask>());
-
-			/**
-			 * 
-			 * ONLINE GAME TASK
-			 */
-
-			game.getTasks().addAll(finalGameTasks());
-			// game.getTasks().addAll(testTasks());
-
-			saveGameDefinition(game);
-			logger.debug("created game {}", gameId);
-		} else {
-			logger.debug("found game {}", gameId);
-		}
-	}
-
-	private Set<GameTask> testTasks() {
-		Set<GameTask> tasks = new HashSet<GameTask>();
-
-		// week classification
-
-		TaskSchedule schedule = new TaskSchedule();
-		schedule.setCronExpression("0 0 15 18 * *");
-		ClassificationTask task4 = new ClassificationTask(schedule, 1,
-				"green leaves", "week classification green");
-		tasks.add(task4);
-
-		schedule = new TaskSchedule();
-		schedule.setCronExpression("0 5 15 18 * *");
-		ClassificationTask task5 = new ClassificationTask(schedule, 1,
-				"health", "week classification health");
-		tasks.add(task5);
-
-		schedule = new TaskSchedule();
-		schedule.setCronExpression("0 10 15 18 * *");
-		ClassificationTask task6 = new ClassificationTask(schedule, 1, "p+r",
-				"week classification p+r");
-		tasks.add(task6);
-
-		// final classification
-
-		schedule = new TaskSchedule();
-		schedule.setCronExpression("0 0 12 19 * *");
-		ClassificationTask task1 = new ClassificationTask(schedule, 3,
-				"green leaves", "final classification green");
-		tasks.add(task1);
-
-		schedule = new TaskSchedule();
-		schedule.setCronExpression("0 5 12 19 * *");
-		ClassificationTask task2 = new ClassificationTask(schedule, 3,
-				"health", "final classification health");
-		tasks.add(task2);
-
-		schedule = new TaskSchedule();
-		schedule.setCronExpression("0 10 12 19 * *");
-		ClassificationTask task3 = new ClassificationTask(schedule, 3, "p+r",
-				"final classification p+r");
-		tasks.add(task3);
-
-		return tasks;
-	}
-
-	private Set<GameTask> finalGameTasks() {
-		Set<GameTask> tasks = new HashSet<GameTask>();
-
-		// week classification
-
-		TaskSchedule schedule = new TaskSchedule();
-		schedule.setCronExpression("0 0 0 1 * *");
-		ClassificationTask task4 = new ClassificationTask(schedule, 1,
-				"green leaves", "week classification green");
-		tasks.add(task4);
-
-		schedule = new TaskSchedule();
-		schedule.setCronExpression("0 5 0 1 * *");
-		ClassificationTask task5 = new ClassificationTask(schedule, 1,
-				"health", "week classification health");
-		tasks.add(task5);
-
-		schedule = new TaskSchedule();
-		schedule.setCronExpression("0 10 0 1 * *");
-		ClassificationTask task6 = new ClassificationTask(schedule, 1, "p+r",
-				"week classification p+r");
-		tasks.add(task6);
-
-		// final classification
-
-		schedule = new TaskSchedule();
-		schedule.setCronExpression("0 10 0 9 * *");
-		ClassificationTask task1 = new ClassificationTask(schedule, 3,
-				"green leaves", "final classification green");
-		tasks.add(task1);
-
-		schedule = new TaskSchedule();
-		schedule.setCronExpression("0 15 0 9 * *");
-		ClassificationTask task2 = new ClassificationTask(schedule, 3,
-				"health", "final classification health");
-		tasks.add(task2);
-
-		schedule = new TaskSchedule();
-		schedule.setCronExpression("0 20 0 9 * *");
-		ClassificationTask task3 = new ClassificationTask(schedule, 3, "p+r",
-				"final classification p+r");
-		tasks.add(task3);
-
-		return tasks;
-
-	}
 
 	public String getGameIdByAction(String actionId) {
 		GamePersistence game = gameRepo.findByActions(actionId);
--- game-engine.workflow/src/test/java/eu/trentorise/game/managers/GameEngineTest.java
+++ game-engine.workflow/src/test/java/eu/trentorise/game/managers/GameEngineTest.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
@@ -18,6 +19,8 @@ import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import eu.trentorise.game.config.AppConfig;
 import eu.trentorise.game.config.MongoConfig;
+import eu.trentorise.game.core.GameTask;
+import eu.trentorise.game.core.TaskSchedule;
 import eu.trentorise.game.model.BadgeCollectionConcept;
 import eu.trentorise.game.model.ClasspathRule;
 import eu.trentorise.game.model.DBRule;
@@ -26,6 +29,7 @@ import eu.trentorise.game.model.GameConcept;
 import eu.trentorise.game.model.PlayerState;
 import eu.trentorise.game.model.PointConcept;
 import eu.trentorise.game.services.GameEngine;
+import eu.trentorise.game.task.ClassificationTask;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { AppConfig.class, MongoConfig.class }, loader = AnnotationConfigContextLoader.class)
@@ -56,7 +60,7 @@ public class GameEngineTest {
 		return game;
 	}
 
-	private void initClasspathGame() {
+	private void initClasspathRuleGame() {
 		mongo.getDb().dropDatabase();
 		gameManager.saveGameDefinition(defineGame());
 
@@ -70,7 +74,7 @@ public class GameEngineTest {
 				+ "/greenPoints.drl"));
 	}
 
-	private void initDBGame() {
+	private void initDBRuleGame() {
 		mongo.getDb().dropDatabase();
 		gameManager.saveGameDefinition(defineGame());
 
@@ -98,15 +102,66 @@ public class GameEngineTest {
 		}
 	}
 
+	/**
+	 * sample usage of classification task
+	 * 
+	 * 
+	 */
+	private Set<GameTask> testTasks() {
+		Set<GameTask> tasks = new HashSet<GameTask>();
+
+		// week classification
+
+		TaskSchedule schedule = new TaskSchedule();
+		schedule.setCronExpression("0 0 15 18 * *");
+		ClassificationTask task4 = new ClassificationTask(schedule, 1,
+				"green leaves", "week classification green");
+		tasks.add(task4);
+
+		schedule = new TaskSchedule();
+		schedule.setCronExpression("0 5 15 18 * *");
+		ClassificationTask task5 = new ClassificationTask(schedule, 1,
+				"health", "week classification health");
+		tasks.add(task5);
+
+		schedule = new TaskSchedule();
+		schedule.setCronExpression("0 10 15 18 * *");
+		ClassificationTask task6 = new ClassificationTask(schedule, 1, "p+r",
+				"week classification p+r");
+		tasks.add(task6);
+
+		// final classification
+
+		schedule = new TaskSchedule();
+		schedule.setCronExpression("0 0 12 19 * *");
+		ClassificationTask task1 = new ClassificationTask(schedule, 3,
+				"green leaves", "final classification green");
+		tasks.add(task1);
+
+		schedule = new TaskSchedule();
+		schedule.setCronExpression("0 5 12 19 * *");
+		ClassificationTask task2 = new ClassificationTask(schedule, 3,
+				"health", "final classification health");
+		tasks.add(task2);
+
+		schedule = new TaskSchedule();
+		schedule.setCronExpression("0 10 12 19 * *");
+		ClassificationTask task3 = new ClassificationTask(schedule, 3, "p+r",
+				"final classification p+r");
+		tasks.add(task3);
+
+		return tasks;
+	}
+
 	@Test
 	public void loadGame() {
-		initClasspathGame();
+		initClasspathRuleGame();
 		Assert.assertEquals(GAME_ID, gameManager.getGameIdByAction(ACTION));
 	}
 
 	@Test
 	public void execution() {
-		initClasspathGame();
+		initClasspathRuleGame();
 		PlayerState p = new PlayerState(PLAYER, GAME_ID);
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("bikeDistance", 8.43);
@@ -137,7 +192,7 @@ public class GameEngineTest {
 
 	@Test
 	public void dbExecution() {
-		initDBGame();
+		initDBRuleGame();
 		PlayerState p = new PlayerState(PLAYER, GAME_ID);
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("bikeDistance", 8.43);

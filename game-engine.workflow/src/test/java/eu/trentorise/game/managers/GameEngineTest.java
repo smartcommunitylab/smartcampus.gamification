522ac0a5bac40f93c328e2e7754576952e7dda5e
mirkoperillo <mirko.perillo@gmail.com>
2015-02-17 17:16:18 +0100

    test improvement

--- game-engine.workflow/src/test/java/eu/trentorise/game/managers/GameEngineTest.java
+++ game-engine.workflow/src/test/java/eu/trentorise/game/managers/GameEngineTest.java
@@ -3,7 +3,9 @@ package eu.trentorise.game.managers;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -17,15 +19,28 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import eu.trentorise.game.config.AppConfig;
 import eu.trentorise.game.config.MongoConfig;
 import eu.trentorise.game.core.AppContextProvider;
 import eu.trentorise.game.core.GameContext;
+import eu.trentorise.game.core.GameTask;
+import eu.trentorise.game.core.TaskSchedule;
 import eu.trentorise.game.model.BadgeCollectionConcept;
 import eu.trentorise.game.model.ClasspathRule;
 import eu.trentorise.game.model.DBRule;
@@ -42,15 +57,17 @@ import eu.trentorise.game.task.ClassificationTask;
 
 /**
  * 
- * Actually three test scenario works only using GameWorkflow implementation
- * (defined in dev profile)
+ * Actually execution tests use Thread.sleep to wait QueueGameWorkflow async
+ * conclusion. This MUST to be fixed, because test result can be machine
+ * dependent.
+ * 
  * 
  * @author mirko perillo
  * 
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { AppConfig.class, MongoConfig.class }, loader = AnnotationConfigContextLoader.class)
-@ActiveProfiles("dev")
+@ContextConfiguration(classes = { AppConfig.class, MongoConfig.class }, loader = AnnotationConfigWebContextLoader.class)
+@WebAppConfiguration
 public class GameEngineTest {
 
 	@Autowired
@@ -60,17 +77,24 @@ public class GameEngineTest {
 	private GameEngine engine;
 
 	@Autowired
-	PlayerService playerSrv;
+	private PlayerService playerSrv;
 
 	@Autowired
-	MongoTemplate mongo;
+	private MongoTemplate mongo;
 
 	@Autowired
-	AppContextProvider provider;
+	private AppContextProvider provider;
+
+	@Autowired
+	private WebApplicationContext wac;
+
+	private MockMvc mocker;
 
 	private static final String GAME = "gameTest";
 	private static final String ACTION = "save_itinerary";
 	private static final String PLAYER = "iansolo";
+
+	private static final long WAIT_EXEC = 15 * 1000;
 
 	@Before
 	public void cleanDB() {
@@ -182,7 +206,7 @@ public class GameEngineTest {
 	}
 
 	@Test
-	public void execution() {
+	public void execution() throws InterruptedException {
 		initClasspathRuleGame();
 		PlayerState p = new PlayerState(PLAYER, GAME);
 		Map<String, Object> params = new HashMap<String, Object>();
@@ -193,6 +217,7 @@ public class GameEngineTest {
 		params.put("p+r", true);
 		params.put("park", "MANIFATTURA");
 		p = engine.execute(GAME, p, ACTION, params);
+		Thread.sleep(WAIT_EXEC);
 		// expected 60 greenPoints and earned 10-point 50-point green badges
 		boolean found = false;
 		for (GameConcept gc : p.getState()) {
@@ -217,7 +242,7 @@ public class GameEngineTest {
 	}
 
 	@Test
-	public void mongoExecution() {
+	public void mongoExecution() throws InterruptedException {
 		initDBRuleGame();
 		PlayerState p = new PlayerState(PLAYER, GAME);
 		Map<String, Object> params = new HashMap<String, Object>();
@@ -229,6 +254,7 @@ public class GameEngineTest {
 		params.put("park", "MANIFATTURA");
 		p = engine.execute(GAME, p, ACTION, params);
 		// expected 60 greenPoints and earned 10-point 50-point green badges
+		Thread.sleep(WAIT_EXEC);
 		boolean found = false;
 		for (GameConcept gc : p.getState()) {
 			if (gc instanceof PointConcept
@@ -318,6 +344,7 @@ public class GameEngineTest {
 		// define game
 		GamePersistence game = defineGame();
 		mongo.save(game);
+
 		gameManager.addRule(new ClasspathRule(GAME, "rules/" + GAME
 				+ "/initState.drl"));
 		gameManager.addRule(new ClasspathRule(GAME, "rules/" + GAME
@@ -606,25 +633,115 @@ public class GameEngineTest {
 	}
 
 	@Test
-	public void simpleScenario() {
+	public void simpleScenario() throws InterruptedException {
 		simpleEnv();
 		launchTaskExecution();
+		Thread.sleep(WAIT_EXEC);
 		analyzeSimple();
 
 	}
 
 	@Test
-	public void sameResultScenario() {
+	public void sameResultScenario() throws InterruptedException {
 		sameResultEnv();
 		launchTaskExecution();
+		Thread.sleep(WAIT_EXEC);
 		analyzeSameResult();
 	}
 
 	@Test
-	public void sameResultLastElementScenario() {
+	public void sameResultLastElementScenario() throws InterruptedException {
 		sameResultLastElementEnv();
 		launchTaskExecution();
+		Thread.sleep(WAIT_EXEC);
 		analyzeSameResultLastResult();
+	}
+
+	@Test
+	public void gameTerminate() {
+		GamePersistence game = defineGame();
+		Calendar cal = new GregorianCalendar();
+		cal.add(Calendar.HOUR_OF_DAY, -2);
+		game.setExpiration(cal.getTimeInMillis());
+		game.setTerminated(false);
+		gameManager.saveGameDefinition(game.toGame());
+		gameManager.taskDestroyer();
+		Game g = gameManager.loadGameDefinitionById(GAME);
+		Assert.assertEquals(true, g.isTerminated());
+
+	}
+
+	@Test
+	public void gameNotTerminate() {
+		GamePersistence game = defineGame();
+		Calendar cal = new GregorianCalendar();
+		cal.add(Calendar.HOUR_OF_DAY, 2);
+		game.setExpiration(cal.getTimeInMillis());
+		game.setTerminated(false);
+		gameManager.saveGameDefinition(game.toGame());
+		gameManager.taskDestroyer();
+		Game g = gameManager.loadGameDefinitionById(GAME);
+		Assert.assertEquals(false, g.isTerminated());
+
+	}
+
+	@Test
+	public void gameEndedRest() throws JsonProcessingException {
+		GamePersistence gp = defineGame();
+		Calendar cal = new GregorianCalendar();
+		cal.add(Calendar.HOUR_OF_DAY, -2);
+		gp.setExpiration(cal.getTimeInMillis());
+		mongo.save(gp);
+		gameManager.taskDestroyer();
+
+		mocker = MockMvcBuilders.webAppContextSetup(wac).build();
+		ObjectMapper mapper = new ObjectMapper();
+		ExecutionData bean = new ExecutionData();
+		bean.setActionId(ACTION);
+		bean.setUserId("1");
+		RequestBuilder builder = MockMvcRequestBuilders.post("/execute")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(mapper.writeValueAsString(bean));
+		try {
+			mocker.perform(builder).andDo(MockMvcResultHandlers.print())
+					.andExpect(MockMvcResultMatchers.status().is(403));
+		} catch (Exception e) {
+			Assert.fail("exception " + e.getMessage());
+		}
+	}
+
+	class ExecutionData {
+		private String actionId;
+		private String userId;
+		private Map<String, Object> data;
+
+		public ExecutionData() {
+		}
+
+		public String getActionId() {
+			return actionId;
+		}
+
+		public void setActionId(String actionId) {
+			this.actionId = actionId;
+		}
+
+		public String getUserId() {
+			return userId;
+		}
+
+		public void setUserId(String userId) {
+			this.userId = userId;
+		}
+
+		public Map<String, Object> getData() {
+			return data;
+		}
+
+		public void setData(Map<String, Object> data) {
+			this.data = data;
+		}
+
 	}
 
 	private void launchTaskExecution() {
@@ -694,39 +811,45 @@ public class GameEngineTest {
 		game.getActions().add(ACTION);
 		game.getActions().add("classification");
 
-		/*
-		 * game.setTasks(new HashSet<GameTask>());
-		 * 
-		 * // final classifications TaskSchedule schedule = new TaskSchedule();
-		 * schedule.setCronExpression("0 20 * * * *"); ClassificationTask task1
-		 * = new ClassificationTask(schedule, 3, "green leaves",
-		 * "final classification green"); game.getTasks().add(task1);
-		 * 
-		 * // schedule = new TaskSchedule(); //
-		 * schedule.setCronExpression("0 * * * * *"); ClassificationTask task2 =
-		 * new ClassificationTask(schedule, 3, "health",
-		 * "final classification health"); game.getTasks().add(task2);
-		 * 
-		 * // schedule = new TaskSchedule(); //
-		 * schedule.setCronExpression("0 * * * * *"); ClassificationTask task3 =
-		 * new ClassificationTask(schedule, 3, "p+r",
-		 * "final classification p+r"); game.getTasks().add(task3);
-		 * 
-		 * // week classifications // schedule = new TaskSchedule(); //
-		 * schedule.setCronExpression("0 * * * * *"); ClassificationTask task4 =
-		 * new ClassificationTask(schedule, 1, "green leaves",
-		 * "week classification green"); game.getTasks().add(task4);
-		 * 
-		 * // schedule = new TaskSchedule(); //
-		 * schedule.setCronExpression("0 * * * * *"); ClassificationTask task5 =
-		 * new ClassificationTask(schedule, 1, "health",
-		 * "week classification health"); game.getTasks().add(task5);
-		 * 
-		 * // schedule = new TaskSchedule(); //
-		 * schedule.setCronExpression("0 * * * * *"); ClassificationTask task6 =
-		 * new ClassificationTask(schedule, 1, "p+r",
-		 * "week classification p+r"); game.getTasks().add(task6);
-		 */
+		game.setTasks(new HashSet<GameTask>());
+
+		// final classifications
+		TaskSchedule schedule = new TaskSchedule();
+		schedule.setCronExpression("0 20 * * * *");
+		ClassificationTask task1 = new ClassificationTask(schedule, 3,
+				"green leaves", "final classification green");
+		game.getTasks().add(task1);
+
+		// schedule = new TaskSchedule(); //
+		schedule.setCronExpression("0 * * * * *");
+		ClassificationTask task2 = new ClassificationTask(schedule, 3,
+				"health", "final classification health");
+		game.getTasks().add(task2);
+
+		// schedule = new TaskSchedule(); //
+		schedule.setCronExpression("0 * * * * *");
+		ClassificationTask task3 = new ClassificationTask(schedule, 3, "p+r",
+				"final classification p+r");
+		game.getTasks().add(task3);
+
+		// week classifications // schedule = new TaskSchedule(); //
+		schedule.setCronExpression("0 * * * * *");
+		ClassificationTask task4 = new ClassificationTask(schedule, 1,
+				"green leaves", "week classification green");
+		game.getTasks().add(task4);
+
+		// schedule = new TaskSchedule(); //
+		schedule.setCronExpression("0 * * * * *");
+		ClassificationTask task5 = new ClassificationTask(schedule, 1,
+				"health", "week classification health");
+		game.getTasks().add(task5);
+
+		// schedule = new TaskSchedule(); //
+		schedule.setCronExpression("0 * * * * *");
+		ClassificationTask task6 = new ClassificationTask(schedule, 1, "p+r",
+				"week classification p+r");
+		game.getTasks().add(task6);
+
 		return new GamePersistence(game);
 
 	}

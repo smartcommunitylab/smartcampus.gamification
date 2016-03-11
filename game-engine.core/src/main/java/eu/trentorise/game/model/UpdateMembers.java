d80acfd6d59a93a0b8bc6ff5c4a4fc05c157049c
mirkoperillo <mirko.perillo@gmail.com>
2016-03-11 15:45:23 +0100

    improved popagation rule simplicity

--- game-engine.core/src/main/java/eu/trentorise/game/model/UpdateMembers.java
+++ game-engine.core/src/main/java/eu/trentorise/game/model/UpdateMembers.java
@@ -1,38 +1,13 @@
 package eu.trentorise.game.model;
 
 public class UpdateMembers {
-	private String teamId;
-	private InputData inputData;
 	private String updateTag;
 
 	public UpdateMembers() {
 	}
 
-	public UpdateMembers(String teamId, InputData inputData) {
-		this.teamId = teamId;
-		this.inputData = inputData;
-	}
-
-	public UpdateMembers(String teamId, InputData inputData, String updateTag) {
-		this.teamId = teamId;
-		this.inputData = inputData;
+	public UpdateMembers(String updateTag) {
 		this.updateTag = updateTag;
-	}
-
-	public String getTeamId() {
-		return teamId;
-	}
-
-	public void setTeamId(String teamId) {
-		this.teamId = teamId;
-	}
-
-	public InputData getInputData() {
-		return inputData;
-	}
-
-	public void setInputData(InputData inputData) {
-		this.inputData = inputData;
 	}
 
 	public String getUpdateTag() {
--- game-engine.core/src/main/java/eu/trentorise/game/model/UpdateTeam.java
+++ game-engine.core/src/main/java/eu/trentorise/game/model/UpdateTeam.java
@@ -1,38 +1,13 @@
 package eu.trentorise.game.model;
 
 public class UpdateTeam {
-	private String playerId;
-	private InputData inputData;
 	private String updateTag;
 
 	public UpdateTeam() {
 	}
 
-	public UpdateTeam(String playerId, InputData inputData) {
-		this.playerId = playerId;
-		this.inputData = inputData;
-	}
-
-	public UpdateTeam(String playerId, InputData inputData, String updateTag) {
-		this.playerId = playerId;
-		this.inputData = inputData;
+	public UpdateTeam(String updateTag) {
 		this.updateTag = updateTag;
-	}
-
-	public String getPlayerId() {
-		return playerId;
-	}
-
-	public void setPlayerId(String playerId) {
-		this.playerId = playerId;
-	}
-
-	public InputData getInputData() {
-		return inputData;
-	}
-
-	public void setInputData(InputData inputData) {
-		this.inputData = inputData;
 	}
 
 	public String getUpdateTag() {

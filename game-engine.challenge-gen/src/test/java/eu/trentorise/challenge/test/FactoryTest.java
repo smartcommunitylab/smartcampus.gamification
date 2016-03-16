4245ce3b8996b1b09d0b55791e17689ff6925cff
albertotn <amartinelli@fbk.eu>
2016-03-16 11:57:45 +0100

    added windows script to insert two users

--- game-engine.challenge-gen/src/test/java/eu/trentorise/challenge/test/FactoryTest.java
+++ game-engine.challenge-gen/src/test/java/eu/trentorise/challenge/test/FactoryTest.java
@@ -11,93 +11,97 @@ import eu.trentorise.game.challenges.model.ChallengeType;
 import eu.trentorise.game.challenges.util.ChallengeFactory;
 
 public class FactoryTest {
-	private ChallengeFactory chFactory;
-	
-	@Test
-	public void buildChallenges() {
-		// simulates Factory client
-		chFactory = new ChallengeFactory();
-		Challenge c = null;
-		
-		//PERCENT Challenge building
-		HashMap<String, Object> params = new HashMap<String, Object>();
-		try {
-				c = chFactory.createChallenge(ChallengeType.PERCENT);
-				params = new HashMap<String, Object>();
-				params.put("percent", new Double(15));
-				params.put("mode", "walkDistance");
-				params.put("bonus", new Integer(50));
-				params.put("point_type", "green leaves");
-				params.put("baseline", new Double(100.0));
-				c.setTemplateParams(params);
-				c.compileChallenge();
-		} catch (UndefinedChallengeException uce) {
-			uce.printStackTrace();
-		}
-		Assert.assertTrue("Challenge " + ChallengeType.PERCENT + " created", c != null);
-		Assert.assertTrue(c.getType().equals(ChallengeType.PERCENT));
-		Assert.assertTrue(c.getGeneratedRules() !=  null && !c.getGeneratedRules().equals(""));
-		System.out.println(c.getGeneratedRules()+ "\n\n");
+    private ChallengeFactory chFactory;
 
-		
-		//TRIPNUMBER Challenge building
-		try {
-			c = chFactory.createChallenge(ChallengeType.TRIPNUMBER);
-			params = new HashMap<String, Object>();
-			params.put("trips", new Integer(1));
-			params.put("mode", "bikesharing");
-			params.put("bonus", new Integer(50));
-			params.put("point_type", "green leaves");
-			c.setTemplateParams(params);
-			c.compileChallenge();
-		} catch (UndefinedChallengeException uce) {
-			// TODO Auto-generated catch block
-			uce.printStackTrace();
-		}
-		Assert.assertTrue("Challenge " + ChallengeType.TRIPNUMBER + " created", c != null);
-		Assert.assertTrue(c.getType().equals(ChallengeType.TRIPNUMBER));
-		Assert.assertTrue(c.getGeneratedRules() !=  null && !c.getGeneratedRules().equals(""));
-		System.out.println(c.getGeneratedRules() + "\n\n");
-		
-		//BADGE COLLECTION COMPLETION Challenge building
-			try {
-				c = chFactory.createChallenge(ChallengeType.BADGECOLLECTION);
-				params = new HashMap<String, Object>();
-				params.put("badge", "badge_foo");
-				params.put("badge_collection", "green leaves");
-				params.put("bonus", new Integer(50));
-				params.put("point_type", "green leaves");
-				c.setTemplateParams(params);
-				c.compileChallenge();
-			} catch (UndefinedChallengeException uce) {
-				// TODO Auto-generated catch block
-				uce.printStackTrace();
-			}
-			Assert.assertTrue("Challenge " + ChallengeType.BADGECOLLECTION + " created", c != null);
-			Assert.assertTrue(c.getType().equals(ChallengeType.BADGECOLLECTION));
-			Assert.assertTrue(c.getGeneratedRules() !=  null && !c.getGeneratedRules().equals(""));
-			System.out.println(c.getGeneratedRules() + "\n\n");		
+    @Test
+    public void buildChallenges() {
+	// simulates Factory client
+	chFactory = new ChallengeFactory();
+	Challenge c = null;
 
-			//GAME RECOMMENDATION Challenge building
-			try {
-				c = chFactory.createChallenge(ChallengeType.RECOMMENDATION);
-				params = new HashMap<String, Object>();
-				params.put("recommendations", new Integer(10));
-				params.put("point_type", "green leaves");
-				params.put("bonus", new Integer(50));
-				c.setTemplateParams(params);
-				c.compileChallenge();
-			} catch (UndefinedChallengeException uce) {
-				// TODO Auto-generated catch block
-				uce.printStackTrace();
-			}
-			Assert.assertTrue("Challenge " + ChallengeType.RECOMMENDATION + " created", c != null);
-			Assert.assertTrue(c.getType().equals(ChallengeType.RECOMMENDATION));
-			Assert.assertTrue(c.getGeneratedRules() !=  null && !c.getGeneratedRules().equals(""));
-			System.out.println(c.getGeneratedRules() + "\n\n");	
+	// PERCENT Challenge building
+	HashMap<String, Object> params = new HashMap<String, Object>();
+	try {
+	    c = chFactory.createChallenge(ChallengeType.PERCENT);
+	    params = new HashMap<String, Object>();
+	    params.put("percent", new Double(15));
+	    params.put("mode", "walkDistance");
+	    params.put("bonus", new Integer(50));
+	    params.put("point_type", "green leaves");
+	    params.put("baseline", new Double(100.0));
+	    c.setTemplateParams(params);
+	    c.compileChallenge();
+	} catch (UndefinedChallengeException uce) {
+	    uce.printStackTrace();
 	}
-}
+	Assert.assertTrue("Challenge " + ChallengeType.PERCENT + " created",
+		c != null);
+	Assert.assertTrue(c.getType().equals(ChallengeType.PERCENT));
+	Assert.assertTrue(c.getGeneratedRules() != null
+		&& !c.getGeneratedRules().equals(""));
+	System.out.println(c.getGeneratedRules() + "\n\n");
 
-	
-	
-	
+	// TRIPNUMBER Challenge building
+	try {
+	    // TODO: cambiare nome per allinearlo a drt? é TravelMode
+	    c = chFactory.createChallenge(ChallengeType.TRIPNUMBER);
+	    params = new HashMap<String, Object>();
+	    params.put("trips", new Integer(1));
+	    params.put("mode", "bikesharing");
+	    params.put("bonus", new Integer(50));
+	    params.put("point_type", "green leaves");
+	    c.setTemplateParams(params);
+	    c.compileChallenge();
+	} catch (UndefinedChallengeException uce) {
+	    // TODO Auto-generated catch block
+	    uce.printStackTrace();
+	}
+	Assert.assertTrue("Challenge " + ChallengeType.TRIPNUMBER + " created",
+		c != null);
+	Assert.assertTrue(c.getType().equals(ChallengeType.TRIPNUMBER));
+	Assert.assertTrue(c.getGeneratedRules() != null
+		&& !c.getGeneratedRules().equals(""));
+	System.out.println(c.getGeneratedRules() + "\n\n");
+
+	// BADGE COLLECTION COMPLETION Challenge building
+	try {
+	    c = chFactory.createChallenge(ChallengeType.BADGECOLLECTION);
+	    params = new HashMap<String, Object>();
+	    params.put("badge", "badge_foo");
+	    params.put("badge_collection", "green leaves");
+	    params.put("bonus", new Integer(50));
+	    params.put("point_type", "green leaves");
+	    c.setTemplateParams(params);
+	    c.compileChallenge();
+	} catch (UndefinedChallengeException uce) {
+	    // TODO Auto-generated catch block
+	    uce.printStackTrace();
+	}
+	Assert.assertTrue("Challenge " + ChallengeType.BADGECOLLECTION
+		+ " created", c != null);
+	Assert.assertTrue(c.getType().equals(ChallengeType.BADGECOLLECTION));
+	Assert.assertTrue(c.getGeneratedRules() != null
+		&& !c.getGeneratedRules().equals(""));
+	System.out.println(c.getGeneratedRules() + "\n\n");
+
+	// GAME RECOMMENDATION Challenge building
+	try {
+	    c = chFactory.createChallenge(ChallengeType.RECOMMENDATION);
+	    params = new HashMap<String, Object>();
+	    params.put("recommendations", new Integer(10));
+	    params.put("point_type", "green leaves");
+	    params.put("bonus", new Integer(50));
+	    c.setTemplateParams(params);
+	    c.compileChallenge();
+	} catch (UndefinedChallengeException uce) {
+	    // TODO Auto-generated catch block
+	    uce.printStackTrace();
+	}
+	Assert.assertTrue("Challenge " + ChallengeType.RECOMMENDATION
+		+ " created", c != null);
+	Assert.assertTrue(c.getType().equals(ChallengeType.RECOMMENDATION));
+	Assert.assertTrue(c.getGeneratedRules() != null
+		&& !c.getGeneratedRules().equals(""));
+	System.out.println(c.getGeneratedRules() + "\n\n");
+    }
+}

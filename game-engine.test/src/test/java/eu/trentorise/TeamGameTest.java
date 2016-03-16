2cd8db5d074c202b932997372fefcc1e03685ee8
mirkoperillo <mirko.perillo@gmail.com>
2016-03-16 16:11:03 +0100

    simplify game and test team hierarchy improvement

--- game-engine.test/src/test/java/eu/trentorise/TeamGameTest.java
+++ game-engine.test/src/test/java/eu/trentorise/TeamGameTest.java
@@ -58,22 +58,22 @@ public class TeamGameTest extends GameTest {
 	@Override
 	public void defineExecData(List<ExecData> execList) {
 		Map<String, Object> data = new HashMap<String, Object>();
-		data.put("meters-walked", 500d); // 750steps
+		data.put("meters-walked", 500d);
 		ExecData input = new ExecData(GAME, ACTION, "prowler", data);
 		execList.add(input);
 
 		data = new HashMap<String, Object>();
-		data.put("meters-walked", 1000d); // 1500steps
+		data.put("meters-walked", 1000d);
 		input = new ExecData(GAME, ACTION, "fuorilegge", data);
 		execList.add(input);
 
 		data = new HashMap<String, Object>();
-		data.put("meters-walked", 400d); // 600steps
+		data.put("meters-walked", 400d);
 		input = new ExecData(GAME, ACTION, "prowler", data);
 		execList.add(input);
 
 		data = new HashMap<String, Object>();
-		data.put("meters-walked", 100d); // 150steps
+		data.put("meters-walked", 100d);
 		input = new ExecData(GAME, ACTION, "rocket racer", data);
 		execList.add(input);
 
@@ -81,15 +81,11 @@ public class TeamGameTest extends GameTest {
 
 	@Override
 	public void analyzeResult() {
-		// assertionPoint(GAME, 1501d, "prowler", "steps");
-		// assertionPoint(GAME, 301d, "rocket racer", "steps");
-		// assertionPoint(GAME, 2270d, "fuorilegge", "steps");
-
-		assertionPoint(GAME, 1501d, "prowler", "steps");
-		assertionPoint(GAME, 301d, "rocket racer", "steps");
-		assertionPoint(GAME, 1685d, "fuorilegge", "steps");
-
-		// assertionPoint(GAME, 2270d, "marvel", "steps");
+		assertionPoint(GAME, 1001d, "prowler", "steps");
+		assertionPoint(GAME, 201d, "rocket racer", "steps");
+		assertionPoint(GAME, 1531d, "fuorilegge", "steps");
+		assertionPoint(GAME, 510d, "marvel", "steps");
+		assertionPoint(GAME, 1d, "secret avengers", "steps");
 
 	}
 

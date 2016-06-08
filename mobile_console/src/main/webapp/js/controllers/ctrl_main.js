04b121f152650c8c9045e4c414213228244166b9
Mattia Bortolamedi <regolo85@gmail.com>
2016-06-08 17:25:26 +0200

    Updated survey data check only if we are in week 9

--- mobile_console/src/main/webapp/js/controllers/ctrl_main.js
+++ mobile_console/src/main/webapp/js/controllers/ctrl_main.js
@@ -954,7 +954,7 @@ cp.controller('MainCtrl',['$scope', '$http', '$route', '$routeParams', '$rootSco
     				// manage Nick for player
     				$scope.retrieveMailForPlayer();
     			}
-    			if((surveyData == null || surveyData == "") && now.getTime() > $scope.start_week_8){	//TODO: change it to 9
+    			if((surveyData == null || surveyData == "") && now.getTime() > $scope.start_week_9){	//TODO: change it to 9
     				// manage SurveyData for player
     				$scope.retrieveSurveyDataForPlayer();
     			}

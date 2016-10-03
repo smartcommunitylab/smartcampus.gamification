8a0494b6695c50c7948028869ebd7500f7cf701b
Mattia Bortolamedi <regolo85@gmail.com>
2016-10-03 16:34:16 +0200

    New class for encrypt and decrypt strings

---
+++ mobile_console/src/main/java/eu/trentorise/smartcampus/gamification_web/service/EncryptDecrypt.java
@@ -0,0 +1,75 @@
+package eu.trentorise.smartcampus.gamification_web.service;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class EncryptDecrypt {
+
+	private static final String SECRET_KEY_1 = "ssdkF$HUy2A#D%kb";
+    private static final String SECRET_KEY_2 = "weJiSEvR5yAC5feB";
+ 
+    private IvParameterSpec ivParameterSpec;
+    private SecretKeySpec secretKeySpec;
+    private Cipher cipher;
+ 
+    public EncryptDecrypt() throws UnsupportedEncodingException, NoSuchPaddingException, NoSuchAlgorithmException {
+        ivParameterSpec = new IvParameterSpec(SECRET_KEY_1.getBytes("UTF-8"));
+        secretKeySpec = new SecretKeySpec(SECRET_KEY_2.getBytes("UTF-8"), "AES");
+        cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+    }
+ 
+ 
+    /**
+     * Encrypt the string with this internal algorithm.
+     *
+     * @param toBeEncrypt string object to be encrypt.
+     * @return returns encrypted string.
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidAlgorithmParameterException
+     * @throws InvalidKeyException
+     * @throws BadPaddingException
+     * @throws IllegalBlockSizeException
+     * @throws UnsupportedEncodingException 
+     */
+    public String encrypt(String toBeEncrypt) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+        byte[] encrypted = cipher.doFinal(toBeEncrypt.getBytes());
+        //byte[] encryptedByteValue = new Base64().encode(encrypted);
+        //String encryptedValue = new String(encryptedByteValue, "UTF-8");
+        return Base64.encodeBase64URLSafeString(encrypted);	//encryptedValue
+    }
+ 
+    /**
+     * Decrypt this string with the internal algorithm. The passed argument should be encrypted using
+     * {@link #encrypt(String) encrypt} method of this class.
+     *
+     * @param encrypted encrypted string that was encrypted using {@link #encrypt(String) encrypt} method.
+     * @return decrypted string.
+     * @throws InvalidAlgorithmParameterException
+     * @throws InvalidKeyException
+     * @throws BadPaddingException
+     * @throws IllegalBlockSizeException
+     */
+    public String decrypt(String encrypted) throws InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+        //byte[] decodedValue = new Base64().decode(encrypted.getBytes());
+        //byte[] decryptedBytes = cipher.doFinal(decodedValue);
+        byte[] decryptedBytes = cipher.doFinal(Base64.decodeBase64(encrypted));
+        return new String(decryptedBytes);
+    }
+
+}

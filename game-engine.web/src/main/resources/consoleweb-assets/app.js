2497788fee8eff435639917bf12a14666517eea1
Nawaz Khurshid <nawaz1981@gmail.com>
2017-01-24 18:14:04 +0100

    Added script for step2 + fixes in controller.js

--- game-engine.web/src/main/resources/consoleweb-assets/app.js
+++ game-engine.web/src/main/resources/consoleweb-assets/app.js
@@ -70,3 +70,21 @@ var app = angular.module('gamificationEngine', [
 		$urlRouterProvider.otherwise("/home");
 	}
 );
+
+app.filter('orderObjectBy', function(){
+	 return function(input, attribute) {
+	    if (!angular.isObject(input)) return input;
+
+	    var array = [];
+	    for(var objectKey in input) {
+	        array.push(input[objectKey]);
+	    }
+
+	    array.sort(function(a, b){
+	        a = parseInt(a[attribute]);
+	        b = parseInt(b[attribute]);
+	        return b - a;
+	    });
+	    return array;
+	 }
+	});

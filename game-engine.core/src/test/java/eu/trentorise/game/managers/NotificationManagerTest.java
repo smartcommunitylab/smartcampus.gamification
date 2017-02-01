ed59cbe7ff2b682f269c636f23a5ed8ec1394d1e
mirkoperillo <mirko.perillo@gmail.com>
2017-02-01 10:33:59 +0100

    fix MessageNotification deserialization issue and added unit test

---
+++ game-engine.core/src/test/java/eu/trentorise/game/managers/NotificationManagerTest.java
@@ -0,0 +1,48 @@
+package eu.trentorise.game.managers;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import eu.trentorise.game.config.AppConfig;
+import eu.trentorise.game.config.MongoConfig;
+import eu.trentorise.game.model.core.Notification;
+import eu.trentorise.game.notification.MessageNotification;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { AppConfig.class, MongoConfig.class }, loader = AnnotationConfigContextLoader.class)
+public class NotificationManagerTest {
+
+	@Autowired
+	private NotificationManager notificationManager;
+
+	@Autowired
+	private MongoTemplate mongo;
+
+	@Before
+	public void cleanDB() {
+		// clean mongo
+		mongo.getDb().dropDatabase();
+	}
+
+	@Test
+	public void messageNotification() {
+		MessageNotification message = new MessageNotification("TEST", "1000",
+				"my_key");
+		message.addData("payload", "I\'m the payload");
+		notificationManager.notificate(message);
+
+		List<Notification> notifications = notificationManager
+				.readNotifications("TEST");
+		Assert.assertEquals(1, notifications.size());
+	}
+
+}

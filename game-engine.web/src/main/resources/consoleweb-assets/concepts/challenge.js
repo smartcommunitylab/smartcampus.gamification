94d1417eebbac394ce187b5a5571188915effc5b
Nawaz Khurshid <nawaz1981@gmail.com>
2017-02-07 17:22:31 +0100

    Added confirmation dialog before delete challenge operation.

--- game-engine.web/src/main/resources/consoleweb-assets/concepts/challenge.js
+++ game-engine.web/src/main/resources/consoleweb-assets/concepts/challenge.js
@@ -29,14 +29,55 @@ concepts.controller('ChallengeCtrl', function ($scope, $rootScope, $timeout, $ui
 		};
 		
 		$scope.deleteModel = function(idx) {
-			gamesFactory.deleteChallengeModel($rootScope.currentGameId, $scope.savedChallenges[idx].id).then(function (instance) {
-				$scope.savedChallenges.splice(idx,1);
-			}, function (message) {
-				console.log("Challenge model failure");
+			// Delete a challenge.
+			var modalInstance = $uibModal.open({
+				templateUrl: 'modals/modal_delete_confirm.html',
+				controller: 'DeleteConceptConfirmModalInstanceCtrl',
+				backdrop: "static",
+				resolve: {
+					game: function () {
+						return $scope.game;
+					},
+					challengeId: function () {
+						return idx;
+					},
+					savedChallenges: function () {
+						return $scope.savedChallenges;
+					}
+					,
+					argument:  function () {
+						return $scope.savedChallenges[idx].name;
+					}
+				}
 			});
-		};
-		
+		}
+
 		function initChallenge() {
 			return {name:'', fields: []};
 		}
 	});
+
+modals.controller('DeleteConceptConfirmModalInstanceCtrl', function ($scope, $rootScope, $uibModalInstance, game, challengeId, savedChallenges, argument, gamesFactory) {
+	$scope.argument = argument;
+
+	$scope.alerts = {
+		'deleteError': false
+	};
+
+	// DELETE button click event-handler
+	$scope.delete = function () {
+		gamesFactory.deleteChallengeModel($rootScope.currentGameId, savedChallenges[challengeId].id).then(
+				function (success) {
+					savedChallenges.splice(challengeId,1);
+					$uibModalInstance.close();
+				}
+				, function (message) {
+					console.log("Challenge delete operation failed");	
+				});
+	};
+
+	// CANCEL button click event-handler
+	$scope.cancel = function () {
+		$uibModalInstance.dismiss('cancel');
+	};
+});

fdffc631db423c09bb44eb2257c56957fa60998a
Leonelli <matteoleonelli99@gmail.com>
2017-06-12 15:10:26 +0200

    fine linea action / inizio elaborazione json badges

--- game-engine.tools/converter-logs/src/main/java/it/smartcommunitylab/gamification/log_converter/manager/RecordManager.java
+++ game-engine.tools/converter-logs/src/main/java/it/smartcommunitylab/gamification/log_converter/manager/RecordManager.java
@@ -3,11 +3,40 @@ package it.smartcommunitylab.gamification.log_converter.manager;
 import it.smartcommunitylab.gamification.log_converter.beans.Record;
 import it.smartcommunitylab.gamification.log_converter.beans.RecordType;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class RecordManager {
 
+	// metodo per gson
+	public String parse(String jsonLine) {
+		JsonElement jelement = new JsonParser().parse(jsonLine);
+		System.out.println("PARSE");
+		// errore.../eccezione
+		JsonObject jobject = jelement.getAsJsonObject();
+		jobject = jobject.getAsJsonObject("data");
+		JsonArray jarray = jobject.getAsJsonArray("translations");
+		jobject = jarray.get(0).getAsJsonObject();
+		String result = jobject.get("name").toString();
+		System.out.println(result);
+		return result;
+	}
+
 	private final static Logger logger = Logger.getLogger(RecordManager.class);
+
+	Map<String, String> dictionary = new HashMap<String, String>();
+	Gson gson = new GsonBuilder().create();
 
 	public RecordManager() {
 	}
@@ -44,9 +73,9 @@ public class RecordManager {
 				record.getIndexType());
 
 		String[] campi = { "type=", "action=", "internalData=", "oldState=" };
-		int[] indiciCampi = new int[4];
-		int[] indiciInformazioni = new int[4];
-		String[] info = new String[4];
+		int[] indiciCampi = new int[campi.length];
+		int[] indiciInformazioni = new int[campi.length];
+		String[] info = new String[campi.length];
 
 		// idicizzazione
 		for (int i = 0; i < campi.length; i++) {
@@ -85,14 +114,16 @@ public class RecordManager {
 	public String analizzaAction(Record record) {
 		String out = null;
 
+		dictionary.clear();
+
 		String splitXSpazi = record.getContent().substring(0,
 				record.getIndexType());
 		String splitDiverso = record.getContent().substring(
 				record.getIndexType());
 
 		String[] campi = { "type=", "action=", "payload=", "oldState=" };
-		int[] indiciCampi = new int[4];
-		int[] indiciInformazioni = new int[4];
+		int[] indiciCampi = new int[campi.length];
+		int[] indiciInformazioni = new int[campi.length];
 		String[] info = new String[4];
 
 		for (int i = 0; i < campi.length; i++) {
@@ -110,11 +141,61 @@ public class RecordManager {
 			} else {
 				info[i] = splitDiverso.substring(indiciCampi[i],
 						splitDiverso.length() - 1);
-				System.out.println("accorcio?");
+
 			}
 
 		}
+		// /////////////////////////
+		// MEMORIZZAZIONE VARIABILI
+		// pulizia json
+		System.out.println("indice campi 3 : " + indiciCampi[3]);
+		String json = info[3].substring(campi[3].length() + 1);// ,
+		String tmpjson = "";
+		// per rimuovere i backslash dal json
+		for (char c : json.toCharArray()) {
+			if (c != '\\') {
+				tmpjson += c;
+			}
+		}
+		json = "{" + tmpjson + "}";
+		System.out.println("json passato: " + json);
+
+		// String nuovoJson = null;
+
+		String test = json.substring(json.indexOf("{"), json.indexOf("}") + 1);
+		System.out.println("test : " + test);
+
+		//
+		JSONParser parser = new JSONParser();
+
+		JSONObject g = new JSONObject(dictionary);
+		System.out.println("g = " + g);
+
+		/*
+		 * try { // JSONObject jsonObject = new JSONObject(); JsonObject
+		 * jsonObject = new JsonParser().parse(json) .getAsJsonObject();
+		 * System.out.println(jsonObject); // String name = (String)
+		 * jsonObject.get("name"); // JSONArray badgesList = (JSONArray)
+		 * jsonObject.get("badgeEarned"); // System.out.println("Game: " +
+		 * name); System.out.println("\nBadges:"); //
+		 * 
+		 * @SuppressWarnings("unchecked") // Iterator<String> iterator =
+		 * badgesList.iterator(); // while (iterator.hasNext()) { //
+		 * System.out.println(iterator.next()); // }
+		 * 
+		 * } catch (Exception e) { e.printStackTrace(); }
+		 */
+		//
+
+		// System.out.println("nuovo json: " + parse(test));// ,
+		// "public transport aficionado"));
+		System.out.println("GSON : " + gson);
+
+		// dictionary.put("key", "value");
+		// String value = dictionary.get("key");
+		// //////////////////////////////////////////////
 		out = splitXSpazi + info[0] + info[1];
+		out = out.substring(0, out.length() - 1);
 		logger.info("il nuovo messaggio per action �: " + out);
 		return out;
 	}

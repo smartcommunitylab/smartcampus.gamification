259d73450c2812eb871f44f14e530e9704f21984
Leonelli <matteoleonelli99@gmail.com>
2017-06-23 14:46:45 +0200

    inserimento in elastic di action e point

---
+++ game-engine.tools/log2elastic/src/main/java/it/smartcommunitylab/gamification/log2elastic/Record.java
@@ -0,0 +1,35 @@
+package it.smartcommunitylab.gamification.log2elastic;
+
+public class Record {
+	private RecordType type;
+	private String content;
+	private int indexType;
+
+	public int getIndexType() {
+		return indexType;
+	}
+
+	public void setIndexType(int indexType) {
+		this.indexType = indexType;
+	}
+
+	public Record() {
+	}
+
+	public RecordType getType() {
+		return type;
+	}
+
+	public void setType(RecordType type) {
+		this.type = type;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+}
---
+++ game-engine.tools/log2elastic/src/main/java/it/smartcommunitylab/gamification/log2elastic/RecordType.java
@@ -0,0 +1,5 @@
+package it.smartcommunitylab.gamification.log2elastic;
+
+public enum RecordType {
+	ACTION, RULE_POINTCONCEPT, RULE_BADGECOLLECTIONCONCEPT, CLASSIFICATION, CHALLENGECOMPLETE, USERCREATION
+}

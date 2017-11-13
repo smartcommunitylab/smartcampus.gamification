9782d427c8265a2580d3bf83faaa6252f0ef3852
mirkoperillo <mirko.perillo@gmail.com>
2017-11-13 13:57:05 +0100

    refactor and added run options

--- game-engine.tools/challenge-db-stats/src/main/java/it/smartcommunitylab/gamification/challenge_db_stats/Application.java
+++ game-engine.tools/challenge-db-stats/src/main/java/it/smartcommunitylab/gamification/challenge_db_stats/Application.java
@@ -18,6 +18,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.apache.log4j.Logger;
 import org.bson.Document;
 
@@ -29,14 +34,8 @@ import com.mongodb.client.MongoDatabase;
 public class Application {
     private static final Logger logger = Logger.getLogger(Application.class);
 
-    /**
-     * 
-     * @param args: a0 -> path of log folder, a1 -> gameId, a2 -> mongoHost, a3 -> mongoPort, a4 ->
-     *        dbName, a5 -> search for assigned challenges (true by default) , a6 -> search for
-     *        completed challenges (true by default)
-     * 
-     */
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws ParseException {
         ApplicationContext context = new ApplicationContext(args);
         logger.info("cartella dei file di log: " + context.getLogFolderPath());
         logger.info(String.format("host: %s port: %s db-name: %s", context.getMongoHost(),
@@ -44,7 +43,6 @@ public class Application {
         creaLoggerChallenge(context);
     }
 
-    @SuppressWarnings("unchecked")
     public static void creaLoggerChallenge(ApplicationContext context) {
         MongoClient mongoClient = new MongoClient(context.getMongoHost(), context.getMongoPort());
         MongoDatabase db = mongoClient.getDatabase(context.getDbName());
@@ -63,7 +61,6 @@ public class Application {
             int contSfidaAssegnata = 0;
             int righeDiLog = 0;
 
-            // Object gameId = player.get("gameId");
             Object playerId = player.get("playerId");
             Map<String, Object> campi = player.get("concepts", Map.class);
 
@@ -85,7 +82,8 @@ public class Application {
                         Map<String, Object> challengeFields = new HashMap<>();
                         challengeFields.put("dateCompleted", obj.get("dateCompleted"));
                         challengeFields.put("start", obj.get("start"));
-                        challengeConcepts.put("end", obj.get("end"));
+                        challengeFields.put("end", obj.get("end"));
+                        challengeFields.put("challengeName", challengeName);
 
 
                         Challenge challenge = instanceOfChallenge(context.getGameId(),
@@ -93,36 +91,41 @@ public class Application {
                         data = dataFormat.format(challenge.getStart());
                         logger.debug(String.format("data di inizio della challenge %s", data));
 
-                        String sfidaAssegnata = challenge.toAssignedLogFormat();
-                        logger.debug("sfida assegnata: " + sfidaAssegnata);
-                        if (scritturaSuLog(challenge.getStart().getTime(), sfidaAssegnata,
-                                context.getLogFolderPath())) {
-                            logger.info("sfida assignata scritta su log con successo su data "
-                                    + Instant.ofEpochMilli(challenge.getStart().getTime())
-                                            .atZone(ZoneId.systemDefault()).toLocalDate()
-                                            .format(DateTimeFormatter.ofPattern("YYYY-MM-dd")));
-                            righeDiLog++;
-                        }
-
-                        if (challenge.isCompleted()) {
-                            contChallengeCompleted++;
-
-                            String sfidaCompletata = challenge.toCompletedLogFormat();
-                            logger.debug("sfida completata: " + sfidaCompletata);
-
-                            if (scritturaSuLog(challenge.getDateCompleted().getTime(),
-                                    sfidaCompletata, context.getLogFolderPath())) {
-                                logger.info("sfida completata scritta su log con successo su data "
-                                        + Instant
-                                                .ofEpochMilli(
-                                                        challenge.getDateCompleted().getTime())
+                        if (context.isConvertAssignedChallenges()) {
+                            String sfidaAssegnata = challenge.toAssignedLogFormat();
+                            logger.debug("sfida assegnata: " + sfidaAssegnata);
+                            if (scritturaSuLog(context, challenge.getStart().getTime(),
+                                    sfidaAssegnata, context.getLogFolderPath())) {
+                                logger.info("sfida assignata scritta su log con successo su data "
+                                        + Instant.ofEpochMilli(challenge.getStart().getTime())
                                                 .atZone(ZoneId.systemDefault()).toLocalDate()
                                                 .format(DateTimeFormatter.ofPattern("YYYY-MM-dd")));
                                 righeDiLog++;
                             }
+                        }
+                        if (context.isConvertCompletedChallenges()) {
+                            if (challenge.isCompleted()) {
+                                contChallengeCompleted++;
 
-                        } else {
-                            logger.debug("sfida non completata: " + challengeName);
+                                String sfidaCompletata = challenge.toCompletedLogFormat();
+                                logger.debug("sfida completata: " + sfidaCompletata);
+
+                                if (scritturaSuLog(context, challenge.getDateCompleted().getTime(),
+                                        sfidaCompletata, context.getLogFolderPath())) {
+                                    logger.info(
+                                            "sfida completata scritta su log con successo su data "
+                                                    + Instant
+                                                            .ofEpochMilli(challenge
+                                                                    .getDateCompleted().getTime())
+                                                            .atZone(ZoneId.systemDefault())
+                                                            .toLocalDate().format(DateTimeFormatter
+                                                                    .ofPattern("YYYY-MM-dd")));
+                                    righeDiLog++;
+                                }
+
+                            } else {
+                                logger.debug("sfida non completata: " + challengeName);
+                            }
                         }
                     }
 
@@ -148,9 +151,12 @@ public class Application {
 
     private static Challenge instanceOfChallenge(String gameId, String playerId,
             Map<String, Object> fields) {
-        return new Challenge(gameId, playerId, (String) fields.get("challengeName"),
-                (Date) fields.get("start"), (Date) fields.get("end"),
-                (Date) fields.get("dateCompleted"));
+        Date start = fields.get("start") != null ? new Date((Long) fields.get("start")) : null;
+        Date end = fields.get("end") != null ? new Date((Long) fields.get("end")) : null;
+        Date dateCompleted = fields.get("dateCompleted") != null
+                ? new Date((Long) fields.get("dateCompleted")) : null;
+        return new Challenge(gameId, playerId, (String) fields.get("challengeName"), start, end,
+                dateCompleted);
     }
 
     private static String getLogFileName(long dateTimestamp) {
@@ -164,66 +170,77 @@ public class Application {
         }
     }
 
-    private static boolean scritturaSuLog(long dateTimestamp, String out, String logfolderPath) {
+    private static boolean scritturaSuLog(ApplicationContext context, long dateTimestamp,
+            String out, String logfolderPath) {
         String logFilename = getLogFileName(dateTimestamp);
         logger.debug("search for logFileName: " + logFilename);
         File logFile = new File(logfolderPath, logFilename);
-        if (logFile.exists()) {
-            FileWriter fw = null;
-            BufferedWriter bw = null;
-            PrintWriter pw = null;
-            try {
-                fw = new FileWriter(logFile, true);
-                bw = new BufferedWriter(fw);
-                pw = new PrintWriter(bw);
-                pw.write(out + "\n");
-                pw.flush();
-                return true;
-            } catch (IOException e) {
-                logger.warn(String.format("Fallita scrittura su file %s: %s", logFile.getName(),
-                        e.getMessage()));
-                return false;
-            } finally {
-                try {
-                    if (pw != null) {
-                        pw.close();
-                    }
-                    if (bw != null) {
-                        bw.close();
-                    }
-                    if (fw != null) {
-                        fw.close();
-                    }
-                } catch (IOException e) {
-                    logger.error("Eccezione nella chiusura degli stream di scrittura");
-                }
-
-            }
-        } else {
-            logger.info(String.format("logFile %s non esiste", logFile.getName()));
+        logFile.getParentFile().mkdirs();
+        FileWriter fw = null;
+        BufferedWriter bw = null;
+        PrintWriter pw = null;
+        try {
+            fw = new FileWriter(logFile, !context.isOverwriteConvertedLogFile());
+            bw = new BufferedWriter(fw);
+            pw = new PrintWriter(bw);
+            pw.write(out + "\n");
+            pw.flush();
+            return true;
+        } catch (IOException e) {
+            logger.warn(String.format("Fallita scrittura su file %s: %s", logFile.getName(),
+                    e.getMessage()));
             return false;
+        } finally {
+            try {
+                if (pw != null) {
+                    pw.close();
+                }
+                if (bw != null) {
+                    bw.close();
+                }
+                if (fw != null) {
+                    fw.close();
+                }
+            } catch (IOException e) {
+                logger.error("Eccezione nella chiusura degli stream di scrittura");
+            }
         }
     }
 
     private static class ApplicationContext {
         private String logFolderPath;
         private String gameId;
-        private String mongoHost = "127.0.0.1";
-        private int mongoPort = 27017;
+        private String mongoHost;
+        private int mongoPort;
         private String dbName;
-        private boolean convertAssignedChallenges = true;
-        private boolean convertCompletedChallenges = true;
+        private boolean convertAssignedChallenges;
+        private boolean convertCompletedChallenges;
+        private boolean overwriteConvertedLogFile;
 
-        public ApplicationContext(String[] args) {
-            logFolderPath = args[0];
-            gameId = args[1];
-            mongoHost = args[2];
-            mongoPort = Integer.valueOf(args[3]);
-            dbName = args[4];
-            if (args.length > 5) {
-                convertAssignedChallenges = Boolean.valueOf(args[5]);
-                convertCompletedChallenges = Boolean.valueOf(args[6]);
-            }
+        public ApplicationContext(String[] args) throws ParseException {
+            Options cliOptions = new Options();
+            cliOptions.addOption("logFolderPath", true, "path of log folder");
+            cliOptions.addOption("gameId", true, "the gameId");
+            cliOptions.addOption("mongoHost", true, "host of mongo server");
+            cliOptions.addOption("mongoPort", true, "port of mongo server");
+            cliOptions.addOption("dbName", true, "name of db in mongo");
+            cliOptions.addOption("ChallengeAssigned", false, "search for assigned challenges");
+            cliOptions.addOption("ChallengeCompleted", false, "search for completed challenges");
+            cliOptions.addOption("overwrite", false,
+                    "use the option to overwrite result log file (append is default behaviour)");
+
+            CommandLineParser parser = new DefaultParser();
+            CommandLine commandLine = parser.parse(cliOptions, args);
+
+
+            logFolderPath = commandLine.getOptionValue("logFolderPath");
+            gameId = commandLine.getOptionValue("gameId");
+            mongoHost = commandLine.getOptionValue("mongoHost", "127.0.0.1");
+            mongoPort = Integer.valueOf(commandLine.getOptionValue("mongoPort", "27017"));
+            dbName = commandLine.getOptionValue("dbName");
+            convertAssignedChallenges = commandLine.hasOption("ChallengeAssigned");
+            convertCompletedChallenges = commandLine.hasOption("ChallengeCompleted");
+            overwriteConvertedLogFile = commandLine.hasOption("overwrite");
 
         }
 
@@ -253,6 +270,10 @@ public class Application {
 
         public boolean isConvertCompletedChallenges() {
             return convertCompletedChallenges;
+        }
+
+        public boolean isOverwriteConvertedLogFile() {
+            return overwriteConvertedLogFile;
         }
 
     }

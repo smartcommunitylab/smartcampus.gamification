3e79eba09df310ef56d1c9441071522d940147f1
mirkoperillo <mirko.perillo@gmail.com>
2017-12-15 15:46:51 +0100

    introduced domain in StatsLogger

--- game-engine.core/src/main/java/eu/trentorise/game/core/StatsLogger.java
+++ game-engine.core/src/main/java/eu/trentorise/game/core/StatsLogger.java
@@ -21,9 +21,10 @@ public class StatsLogger {
 
     private static final Logger internalLogger = LoggerFactory.getLogger(StatsLogger.class);
 
-    public static String logRule(String gameId, String playerId, String executionId,
+    public static String logRule(String domain, String gameId, String playerId, String executionId,
             long executionMoment, long timestamp, String ruleName, PointConcept concept) {
-        String msg = commonFieldsOutput(gameId, playerId, executionId, executionMoment, timestamp);
+        String msg = commonFieldsOutput(domain, gameId, playerId, executionId, executionMoment,
+                timestamp);
         msg += " " + String.format("type=%s ruleName=\"%s\" name=\"%s\" score=%s",
                 PointConcept.class.getSimpleName(), ruleName, concept.getName(),
                 concept.getScore());
@@ -31,29 +32,30 @@ public class StatsLogger {
         return msg;
     }
 
-    public static String logRule(String gameId, String playerId, String executionId,
+    public static String logRule(String domain, String gameId, String playerId, String executionId,
             long executionMoment, String ruleName, PointConcept concept) {
-        return logRule(gameId, playerId, executionId, executionMoment, System.currentTimeMillis(),
-                ruleName, concept);
+        return logRule(domain, gameId, playerId, executionId, executionMoment,
+                System.currentTimeMillis(), ruleName, concept);
     }
 
-    public static String logRulePointConceptDelta(String gameId, String playerId,
+    public static String logRulePointConceptDelta(String domain, String gameId, String playerId,
             String executionId, long executionMoment, String ruleName, PointConcept concept,
             double deltaScore) {
-        return logRulePointConceptDelta(gameId, playerId, executionId, executionMoment,
+        return logRulePointConceptDelta(domain, gameId, playerId, executionId, executionMoment,
                 System.currentTimeMillis(), ruleName, concept, deltaScore);
     }
 
-    public static String logRuleBadgeCollectionConceptDelta(String gameId, String playerId,
-            String executionId, long executionMoment, String ruleName, String conceptName,
-            String badge) {
-        return logRuleBadgeCollectionConceptDelta(gameId, playerId, executionId, executionMoment,
-                System.currentTimeMillis(), ruleName, conceptName, badge);
+    public static String logRuleBadgeCollectionConceptDelta(String domain, String gameId,
+            String playerId, String executionId, long executionMoment, String ruleName,
+            String conceptName, String badge) {
+        return logRuleBadgeCollectionConceptDelta(domain, gameId, playerId, executionId,
+                executionMoment, System.currentTimeMillis(), ruleName, conceptName, badge);
     }
 
-    public static String logRule(String gameId, String playerId, String executionId,
+    public static String logRule(String domain, String gameId, String playerId, String executionId,
             long executionMoment, long timestamp, String ruleName, BadgeCollectionConcept concept) {
-        String msg = commonFieldsOutput(gameId, playerId, executionId, executionMoment, timestamp);
+        String msg = commonFieldsOutput(domain, gameId, playerId, executionId, executionMoment,
+                timestamp);
         msg += " " + String.format("type=%s ruleName=\"%s\" name=\"%s\" badges=\"%s\"",
                 BadgeCollectionConcept.class.getSimpleName(), ruleName, concept.getName(),
                 concept.getBadgeEarned());
@@ -61,16 +63,17 @@ public class StatsLogger {
         return msg;
     }
 
-    public static String logRule(String gameId, String playerId, String executionId,
+    public static String logRule(String domain, String gameId, String playerId, String executionId,
             long executionMoment, String ruleName, BadgeCollectionConcept concept) {
-        return logRule(gameId, playerId, executionId, executionMoment, System.currentTimeMillis(),
-                ruleName, concept);
+        return logRule(domain, gameId, playerId, executionId, executionMoment,
+                System.currentTimeMillis(), ruleName, concept);
     }
 
-    private static String logRulePointConceptDelta(String gameId, String playerId,
+    private static String logRulePointConceptDelta(String domain, String gameId, String playerId,
             String executionId, long executionMoment, long timestamp, String ruleName,
             PointConcept concept, double deltaScore) {
-        String msg = commonFieldsOutput(gameId, playerId, executionId, executionMoment, timestamp);
+        String msg = commonFieldsOutput(domain, gameId, playerId, executionId, executionMoment,
+                timestamp);
 
         msg += " " + String.format("type=%s ruleName=\"%s\" name=\"%s\" deltaScore=%s score=%s",
                 PointConcept.class.getSimpleName(), ruleName, concept.getName(), deltaScore,
@@ -80,10 +83,11 @@ public class StatsLogger {
         return msg;
     }
 
-    private static String logRuleBadgeCollectionConceptDelta(String gameId, String playerId,
-            String executionId, long executionMoment, long timestamp, String ruleName,
-            String conceptName, String badge) {
-        String msg = commonFieldsOutput(gameId, playerId, executionId, executionMoment, timestamp);
+    private static String logRuleBadgeCollectionConceptDelta(String domain, String gameId,
+            String playerId, String executionId, long executionMoment, long timestamp,
+            String ruleName, String conceptName, String badge) {
+        String msg = commonFieldsOutput(domain, gameId, playerId, executionId, executionMoment,
+                timestamp);
 
         msg += " " + String.format("type=%s ruleName=\"%s\" name=\"%s\" new_badge=\"%s\"",
                 BadgeCollectionConcept.class.getSimpleName(), ruleName, conceptName, badge);
@@ -91,33 +95,36 @@ public class StatsLogger {
         return msg;
     }
 
-    public static String logAction(String gameId, String playerId, String executionId,
-            long executionMoment, String action, Map<String, Object> inputData,
+    public static String logAction(String domain, String gameId, String playerId,
+            String executionId, long executionMoment, String action, Map<String, Object> inputData,
             List<Object> factObjects, PlayerState state) {
-        return logAction(gameId, playerId, executionId, executionMoment, System.currentTimeMillis(),
-                action, inputData, factObjects, state);
+        return logAction(domain, gameId, playerId, executionId, executionMoment,
+                System.currentTimeMillis(), action, inputData, factObjects, state);
     }
 
-    public static String logAction(String gameId, String playerId, String executionId,
-            long executionMoment, long timestamp, String action, Map<String, Object> inputData,
-            List<Object> factObjects, PlayerState state) {
-        String msg = commonFieldsOutput(gameId, playerId, executionId, executionMoment, timestamp);
+    public static String logAction(String domain, String gameId, String playerId,
+            String executionId, long executionMoment, long timestamp, String action,
+            Map<String, Object> inputData, List<Object> factObjects, PlayerState state) {
+        String msg = commonFieldsOutput(domain, gameId, playerId, executionId, executionMoment,
+                timestamp);
 
         msg += " " + String.format("type=%s actionName=\"%s\"", "Action", action);
         statsLogger.info(msg);
         return msg;
     }
 
-    public static String logClassification(String gameId, String playerId, String executionId,
-            long executionMoment, Map<String, Object> inputData, List<Object> factObjects) {
-        return logClassification(gameId, playerId, executionId, executionMoment,
+    public static String logClassification(String domain, String gameId, String playerId,
+            String executionId, long executionMoment, Map<String, Object> inputData,
+            List<Object> factObjects) {
+        return logClassification(domain, gameId, playerId, executionId, executionMoment,
                 System.currentTimeMillis(), inputData, factObjects);
     }
 
-    public static String logClassification(String gameId, String playerId, String executionId,
-            long executionMoment, long timestamp, Map<String, Object> inputData,
+    public static String logClassification(String domain, String gameId, String playerId,
+            String executionId, long executionMoment, long timestamp, Map<String, Object> inputData,
             List<Object> factObjects) {
-        String msg = commonFieldsOutput(gameId, playerId, executionId, executionMoment, timestamp);
+        String msg = commonFieldsOutput(domain, gameId, playerId, executionId, executionMoment,
+                timestamp);
 
         if (CollectionUtils.isNotEmpty(factObjects)) {
             for (Object factObj : factObjects) {
@@ -138,17 +145,19 @@ public class StatsLogger {
         return "";
     }
 
-    public static String logUserCreation(String gameId, String playerId, String executionId,
-            long timestamp) {
-        String msg = commonFieldsOutput(gameId, playerId, executionId, timestamp, timestamp);
+    public static String logUserCreation(String domain, String gameId, String playerId,
+            String executionId, long timestamp) {
+        String msg =
+                commonFieldsOutput(domain, gameId, playerId, executionId, timestamp, timestamp);
         msg += " " + String.format("type=%s creation", "UserCreation");
         statsLogger.info(msg);
         return msg;
     }
 
-    public static String logChallengeAssignment(String gameId, String playerId, String executionId,
-            long timestamp, String challengeName, Date start, Date end) {
-        String msg = commonFieldsOutput(gameId, playerId, executionId, timestamp, timestamp);
+    public static String logChallengeAssignment(String domain, String gameId, String playerId,
+            String executionId, long timestamp, String challengeName, Date start, Date end) {
+        String msg =
+                commonFieldsOutput(domain, gameId, playerId, executionId, timestamp, timestamp);
         msg += " " + String.format("type=%s name=\"%s\" startDate=%s endDate=%s",
                 "ChallengeAssigned", challengeName, start != null ? start.getTime() : null,
                 end != null ? end.getTime() : null);
@@ -156,26 +165,28 @@ public class StatsLogger {
         return msg;
     }
 
-    public static String logChallengeCompleted(String gameId, String playerId, String executionId,
-            long executionTime, long timestamp, String challengeName) {
-        String msg = commonFieldsOutput(gameId, playerId, executionId, timestamp, timestamp);
+    public static String logChallengeCompleted(String domain, String gameId, String playerId,
+            String executionId, long executionTime, long timestamp, String challengeName) {
+        String msg =
+                commonFieldsOutput(domain, gameId, playerId, executionId, timestamp, timestamp);
         msg += " " + String.format("type=%s name=\"%s\" completed", "ChallengeCompleted",
                 challengeName);
         statsLogger.info(msg);
         return msg;
     }
 
-    public static String logEndGameAction(String gameId, String playerId, String executionId,
-            long executionTime, long timestamp) {
-        String msg = commonFieldsOutput(gameId, playerId, executionId, timestamp, timestamp);
+    public static String logEndGameAction(String domain, String gameId, String playerId,
+            String executionId, long executionTime, long timestamp) {
+        String msg =
+                commonFieldsOutput(domain, gameId, playerId, executionId, timestamp, timestamp);
         msg += " " + String.format("type=%s", "EndGameAction");
         statsLogger.info(msg);
         return msg;
     }
 
-    private static String commonFieldsOutput(String gameId, String playerId, String executionId,
-            long executionMoment, long timestamp) {
-        return String.format("\"%s\" \"%s\" %s %s %s", gameId, playerId, executionId,
+    private static String commonFieldsOutput(String domain, String gameId, String playerId,
+            String executionId, long executionMoment, long timestamp) {
+        return String.format("\"%s\" \"%s\" \"%s\" %s %s %s", domain, gameId, playerId, executionId,
                 executionMoment, timestamp);
     }
 }
--- game-engine.core/src/test/java/eu/trentorise/game/core/StatsLoggerTest.java
+++ game-engine.core/src/test/java/eu/trentorise/game/core/StatsLoggerTest.java
@@ -15,10 +15,10 @@ public class StatsLoggerTest {
     public void assign_challenge_with_start_date_null() {
         Date endDate = new LocalDateTime(2017, 10, 22, 0, 0).toDate();
         long timestamp = 10l;
-        String message = StatsLogger.logChallengeAssignment("GAME", "PLAYER", "EXEC_ID", timestamp,
-                "CHALLENGE_NAME", null, endDate);
+        String message = StatsLogger.logChallengeAssignment("DOMAIN", "GAME", "PLAYER", "EXEC_ID",
+                timestamp, "CHALLENGE_NAME", null, endDate);
         String expectedMessage =
-                "\"GAME\" \"PLAYER\" EXEC_ID 10 10 type=ChallengeAssigned name=\"CHALLENGE_NAME\" startDate=null endDate=1508623200000";
+                "\"DOMAIN\" \"GAME\" \"PLAYER\" EXEC_ID 10 10 type=ChallengeAssigned name=\"CHALLENGE_NAME\" startDate=null endDate=1508623200000";
         Assert.assertThat(message, Matchers.is(expectedMessage));
     }
 
@@ -26,19 +26,19 @@ public class StatsLoggerTest {
     public void assign_challenge_with_end_date_null() {
         Date startDate = new LocalDateTime(2017, 10, 22, 0, 0).toDate();
         long timestamp = 10l;
-        String message = StatsLogger.logChallengeAssignment("GAME", "PLAYER", "EXEC_ID", timestamp,
-                "CHALLENGE_NAME", startDate, null);
+        String message = StatsLogger.logChallengeAssignment("DOMAIN", "GAME", "PLAYER", "EXEC_ID",
+                timestamp, "CHALLENGE_NAME", startDate, null);
         String expectedMessage =
-                "\"GAME\" \"PLAYER\" EXEC_ID 10 10 type=ChallengeAssigned name=\"CHALLENGE_NAME\" startDate=1508623200000 endDate=null";
+                "\"DOMAIN\" \"GAME\" \"PLAYER\" EXEC_ID 10 10 type=ChallengeAssigned name=\"CHALLENGE_NAME\" startDate=1508623200000 endDate=null";
         Assert.assertThat(message, Matchers.is(expectedMessage));
     }
 
     @Test
     public void assign_challenge_always_valid() {
-        String message = StatsLogger.logChallengeAssignment("GAME", "PLAYER", "EXEC_ID", 10l,
-                "CHALLENGE_NAME", null, null);
+        String message = StatsLogger.logChallengeAssignment("DOMAIN", "GAME", "PLAYER", "EXEC_ID",
+                10l, "CHALLENGE_NAME", null, null);
         String expectedMessage =
-                "\"GAME\" \"PLAYER\" EXEC_ID 10 10 type=ChallengeAssigned name=\"CHALLENGE_NAME\" startDate=null endDate=null";
+                "\"DOMAIN\" \"GAME\" \"PLAYER\" EXEC_ID 10 10 type=ChallengeAssigned name=\"CHALLENGE_NAME\" startDate=null endDate=null";
         Assert.assertThat(message, Matchers.is(expectedMessage));
     }
 
@@ -48,10 +48,10 @@ public class StatsLoggerTest {
                 LocalDate.parse("2017-10-12").atStartOfDay(ZoneId.systemDefault()).toInstant());
         Date end = Date.from(
                 LocalDate.parse("2017-10-13").atStartOfDay(ZoneId.systemDefault()).toInstant());
-        String message = StatsLogger.logChallengeAssignment("GAME", "PLAYER", "EXEC_ID", 10l,
-                "CHALLENGE_NAME", start, end);
+        String message = StatsLogger.logChallengeAssignment("DOMAIN", "GAME", "PLAYER", "EXEC_ID",
+                10l, "CHALLENGE_NAME", start, end);
         String expectedMessage =
-                "\"GAME\" \"PLAYER\" EXEC_ID 10 10 type=ChallengeAssigned name=\"CHALLENGE_NAME\" startDate=1507759200000 endDate=1507845600000";
+                "\"DOMAIN\" \"GAME\" \"PLAYER\" EXEC_ID 10 10 type=ChallengeAssigned name=\"CHALLENGE_NAME\" startDate=1507759200000 endDate=1507845600000";
         Assert.assertThat(message, Matchers.is(expectedMessage));
     }
 }

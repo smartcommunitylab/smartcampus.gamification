58a2e2e86cad10b863d6c819eca7ca10ee326352
mirkoperillo <mirko.perillo@gmail.com>
2018-02-01 15:29:08 +0100

    enabled actuators stuff

--- game-engine.web/src/main/java/eu/trentorise/game/config/WebConfig.java
+++ game-engine.web/src/main/java/eu/trentorise/game/config/WebConfig.java
@@ -1,17 +1,15 @@
 /**
- *    Copyright 2015 Fondazione Bruno Kessler - Trento RISE
+ * Copyright 2015 Fondazione Bruno Kessler - Trento RISE
  *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package eu.trentorise.game.config;
@@ -20,6 +18,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -30,49 +29,49 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 import eu.trentorise.game.api.rest.AuthorizationInterceptor;
 
 /*
- * extend WebMvcConfigurerAdapter and not use annotation @EnableMvc to permit
- * correct static resources publishing and restController functionalities
+ * extend WebMvcConfigurerAdapter and not use annotation @EnableMvc to permit correct static
+ * resources publishing and restController functionalities
  */
 @Configuration
+@PropertySource("classpath:engine.web.properties")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
-	/**
-	 * If this mapping change, remember to align angular file app.js
-	 * i18nextProvider if not angular internationalization will be broken
-	 */
-	private static final String CONSOLE_URL_MAPPING = "consoleweb";
+    /**
+     * If this mapping change, remember to align angular file app.js i18nextProvider if not angular
+     * internationalization will be broken
+     */
+    private static final String CONSOLE_URL_MAPPING = "consoleweb";
 
-	@Autowired
-	AuthorizationInterceptor authInterceptor;
+    @Autowired
+    AuthorizationInterceptor authInterceptor;
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler(
-				String.format("/%s/**", CONSOLE_URL_MAPPING))
-				.addResourceLocations("classpath:/consoleweb-assets/");
-	}
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler(String.format("/%s/**", CONSOLE_URL_MAPPING))
+                .addResourceLocations("classpath:/consoleweb-assets/");
+    }
 
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController(String.format("/%s/", CONSOLE_URL_MAPPING))
-				.setViewName("forward:index.html");
-	}
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController(String.format("/%s/", CONSOLE_URL_MAPPING))
+                .setViewName("forward:index.html");
+    }
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(authInterceptor).addPathPatterns(
-				"/console/game/**", "/model/game/**");
-	}
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor).addPathPatterns("/console/game/**",
+                "/model/game/**");
+    }
 
-	@Override
-	public void addArgumentResolvers(
-			List<HandlerMethodArgumentResolver> argumentResolvers) {
-		PageableHandlerMethodArgumentResolver pageResolver = new PageableHandlerMethodArgumentResolver();
-		pageResolver.setPrefix("");
-		pageResolver.setOneIndexedParameters(true);
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        PageableHandlerMethodArgumentResolver pageResolver =
+                new PageableHandlerMethodArgumentResolver();
+        pageResolver.setPrefix("");
+        pageResolver.setOneIndexedParameters(true);
 
-		argumentResolvers.add(pageResolver);
-		super.addArgumentResolvers(argumentResolvers);
-	}
+        argumentResolvers.add(pageResolver);
+        super.addArgumentResolvers(argumentResolvers);
+    }
 
 }

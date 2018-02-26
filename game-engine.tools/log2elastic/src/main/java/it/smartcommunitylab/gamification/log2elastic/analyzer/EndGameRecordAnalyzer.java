0ad26a7f0d7101800a9045ab98e34497b1cb8387
mirkoperillo <mirko.perillo@gmail.com>
2018-02-26 16:02:08 +0100

    log2elastic: process EndGameAction events

---
+++ game-engine.tools/log2elastic/src/main/java/it/smartcommunitylab/gamification/log2elastic/analyzer/EndGameRecordAnalyzer.java
@@ -0,0 +1,37 @@
+package it.smartcommunitylab.gamification.log2elastic.analyzer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+import it.smartcommunitylab.gamification.log2elastic.Record;
+
+public class EndGameRecordAnalyzer extends BaseRecordAnalyzer {
+
+    private static final Logger logger = Logger.getLogger(UserCreationRecordAnalyzer.class);
+
+    protected EndGameRecordAnalyzer(Record record) {
+        super(record);
+    }
+
+    @Override
+    public Map<String, String> extractData() {
+        String eventType = specificFields.get(campi[0]);
+        logger.debug("eventType: " + eventType);
+
+        Map<String, String> data = new HashMap<>(commonFields);
+        data.put("eventType", eventType);
+        // TODO improvement: eventType has to be moved to commonFields and type field set by
+        // metaInfo
+        data.put("type", eventType);
+        return data;
+    }
+
+    @Override
+    public String[] getNomiCampi() {
+        String[] campi = {"type="};
+        return campi;
+    }
+
+}

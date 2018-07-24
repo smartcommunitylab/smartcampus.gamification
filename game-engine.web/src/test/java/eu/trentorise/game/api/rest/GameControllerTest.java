918b1b7a675c76fa70193326d1db94cb24ae5293
mirkoperillo <mirko.perillo@gmail.com>
2018-07-24 15:58:24 +0200

    add acceptance tests for level APIs

--- game-engine.web/src/test/java/eu/trentorise/game/api/rest/GameControllerTest.java
+++ game-engine.web/src/test/java/eu/trentorise/game/api/rest/GameControllerTest.java
@@ -35,11 +35,19 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import eu.trentorise.game.bean.GameDTO;
+import eu.trentorise.game.bean.LevelDTO;
+import eu.trentorise.game.bean.LevelDTO.ThresholdDTO;
 import eu.trentorise.game.config.AppConfig;
 import eu.trentorise.game.config.MongoConfig;
 import eu.trentorise.game.config.NoSecurityConfig;
 import eu.trentorise.game.model.Game;
+import eu.trentorise.game.model.Level;
+import eu.trentorise.game.model.Level.Threshold;
+import eu.trentorise.game.model.PlayerLevel;
+import eu.trentorise.game.model.PlayerState;
+import eu.trentorise.game.model.PointConcept;
 import eu.trentorise.game.services.GameService;
+import eu.trentorise.game.services.PlayerService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ActiveProfiles({"no-sec"})
@@ -60,17 +68,21 @@ public class GameControllerTest {
     @Autowired
     private MongoTemplate mongo;
 
+    @Autowired
+    private WebApplicationContext wac;
+
+    @Autowired
+    private GameService gameSrv;
+
+    @Autowired
+    private PlayerService playerSrv;
+
 
     @Before
     public void cleanDB() {
         mongo.getDb().dropDatabase();
     }
 
-    @Autowired
-    private WebApplicationContext wac;
-
-    @Autowired
-    private GameService gameSrv;
 
 
 
@@ -180,5 +192,134 @@ public class GameControllerTest {
 
         // check if the game has been deleted
         Assert.assertNull(gameSrv.loadGameDefinitionById(g.getId()));
+    }
+
+    @Test
+    public void define_a_level() throws JsonProcessingException {
+        final String gameId = "MY_GAME";
+        Game g = new Game(gameId);
+        g.setConcepts(new HashSet<>());
+        g.getConcepts().add(new PointConcept("green"));
+        g = gameSrv.saveGameDefinition(g);
+
+        LevelDTO level = new LevelDTO();
+        level.setName("Eco Warrior");
+        level.setPointConceptName("green");
+
+        level.getThresholds().add(threshold("newbie", 0));
+        level.getThresholds().add(threshold("adept", 100));
+        level.getThresholds().add(threshold("master", 1000));
+
+        String levelAsString = mapper.writeValueAsString(level);
+
+        RequestBuilder builder = MockMvcRequestBuilders.post("/model/game/{gameId}/level", gameId)
+                .contentType(MediaType.APPLICATION_JSON).content(levelAsString);
+
+        try {
+            mocker.perform(builder).andExpect(status().is(200))
+                    .andExpect(jsonPath("$.name", is("Eco Warrior")))
+                    .andExpect(jsonPath("$.thresholds", hasSize(3)));
+        } catch (Exception e) {
+            Assert.fail("exception: " + e.getMessage());
+        }
+
+        Game loaded = gameSrv.loadGameDefinitionById(gameId);
+        Assert.assertEquals(1, loaded.getLevels().size());
+        Level loadedLevel = loaded.getLevels().get(0);
+        Assert.assertEquals("Eco Warrior", loadedLevel.getName());
+        Assert.assertEquals(3, loadedLevel.getThresholds().size());
+        Threshold newbie = loadedLevel.getThresholds().get(0);
+        Assert.assertEquals("newbie", newbie.getName());
+
+    }
+
+
+
+    @Test
+    public void delete_a_level() throws JsonProcessingException {
+        final String gameId = "MY_GAME";
+        Game g = new Game(gameId);
+        g.setConcepts(new HashSet<>());
+        g.getConcepts().add(new PointConcept("green"));
+        g = gameSrv.saveGameDefinition(g);
+
+        LevelDTO level = new LevelDTO();
+        level.setName("Eco Warrior");
+        level.setPointConceptName("green");
+
+        level.getThresholds().add(threshold("newbie", 0));
+        level.getThresholds().add(threshold("adept", 100));
+        level.getThresholds().add(threshold("master", 1000));
+
+        String levelAsString = mapper.writeValueAsString(level);
+
+        RequestBuilder builder = MockMvcRequestBuilders.post("/model/game/{gameId}/level", gameId)
+                .contentType(MediaType.APPLICATION_JSON).content(levelAsString);
+
+        try {
+            mocker.perform(builder).andExpect(status().is(200))
+                    .andExpect(jsonPath("$.name", is("Eco Warrior")))
+                    .andExpect(jsonPath("$.thresholds", hasSize(3)));
+        } catch (Exception e) {
+            Assert.fail("exception: " + e.getMessage());
+        }
+
+        RequestBuilder deleteBuilder = MockMvcRequestBuilders
+                .delete("/model/game/{gameId}/level/{levelName}", gameId, "Eco Warrior");
+
+        try {
+            mocker.perform(deleteBuilder).andExpect(status().is(200));
+        } catch (Exception e) {
+            Assert.fail("exception: " + e.getMessage());
+        }
+
+        Game loaded = gameSrv.loadGameDefinitionById(gameId);
+        Assert.assertEquals(0, loaded.getLevels().size());
+
+    }
+
+
+    @Test
+    public void read_player_level() throws JsonProcessingException {
+        final String gameId = "MY_GAME";
+        Game g = new Game(gameId);
+        g.setConcepts(new HashSet<>());
+        g.getConcepts().add(new PointConcept("green"));
+        g = gameSrv.saveGameDefinition(g);
+
+        Level level = new Level("Eco Warrior", "green");
+        level.getThresholds().add(new Threshold("newbie", 0));
+        level.getThresholds().add(new Threshold("adept", 100));
+        level.getThresholds().add(new Threshold("master", 1000));
+
+        gameSrv.upsertLevel(gameId, level);
+
+        PlayerState p = playerSrv.loadState(gameId, "player", true);
+        p.getLevels().add(new PlayerLevel(level, "adept", 700));
+        playerSrv.saveState(p);
+        
+        RequestBuilder getBuilder = MockMvcRequestBuilders
+                .get("/data/game/{gameId}/player/{playerId}/levels", gameId, "player");
+
+        try {
+            mocker.perform(getBuilder).andExpect(status().is(200))
+                    .andExpect(jsonPath("$", hasSize(1)))
+                    .andExpect(jsonPath("$[0].levelName", is("Eco Warrior")))
+                    .andExpect(jsonPath("$[0].levelValue", is("adept")))
+                    .andExpect(jsonPath("$[0].pointConcept", is("green")))
+                    .andExpect(jsonPath("$[0].toNextLevel", is(700d)));
+        } catch (Exception e) {
+            Assert.fail("exception: " + e.getMessage());
+        }
+
+    }
+
+
+    private ThresholdDTO threshold(String name, double value) {
+        ThresholdDTO thres = new ThresholdDTO();
+        thres.setName(name);
+        thres.setValue(value);
+
+        return thres;
     }
 }

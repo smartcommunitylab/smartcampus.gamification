a47c15543f7d3cf41ee2a3e3539e16513f266932
mirkoperillo <mirko.perillo@gmail.com>
2018-10-22 15:53:28 +0200

    invitation WIP

--- game-engine.core/src/main/java/eu/trentorise/game/managers/ChallengeManager.java
+++ game-engine.core/src/main/java/eu/trentorise/game/managers/ChallengeManager.java
@@ -1,6 +1,8 @@
 package eu.trentorise.game.managers;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +12,16 @@ import eu.trentorise.game.core.Clock;
 import eu.trentorise.game.model.ChallengeConcept.ChallengeState;
 import eu.trentorise.game.model.GroupChallenge;
 import eu.trentorise.game.model.GroupChallenge.Attendee;
+import eu.trentorise.game.model.GroupChallenge.Attendee.Role;
+import eu.trentorise.game.model.Invitation;
 import eu.trentorise.game.model.PlayerState;
 import eu.trentorise.game.repo.GroupChallengeRepo;
 import eu.trentorise.game.services.PlayerService;
 
 @Service
 public class ChallengeManager {
+
+    private static final String INVITATION_CHALLENGE_ORIGIN = "player";
 
     @Autowired
     private PlayerService playerSrv;
@@ -51,4 +57,49 @@ public class ChallengeManager {
                 clock.now());
     }
 
+    public void inviteToChallenge(Invitation invitation) {
+        // save GroupChallenge
+        GroupChallenge groupChallenge = convert(invitation);
+
+        // produce notification to other attendees
+        // stats notification
+    }
+
+    private GroupChallenge convert(Invitation invitation) {
+        GroupChallenge challenge = null;
+        if (invitation != null) {
+            challenge = new GroupChallenge(ChallengeState.PROPOSED);
+            challenge.setChallengePointConcept(invitation.getChallengePointConcept());
+            challenge.setOrigin(INVITATION_CHALLENGE_ORIGIN);
+            challenge.setStart(invitation.getChallengeStart());
+            challenge.setEnd(invitation.getChallengeEnd());
+            challenge.setAttendees(attendees(invitation));
+            challenge.setGameId(invitation.getGameId());
+            challenge.setReward(invitation.getReward());
+            challenge.setInstanceName(String.format("p%s_%s",
+                    invitation.getProposer().getPlayerId(), UUID.randomUUID().toString()));
+        }
+
+        return challenge;
+    }
+
+    private List<Attendee> attendees(Invitation invitation) {
+        List<Attendee> attendees = new ArrayList<GroupChallenge.Attendee>();
+        if (invitation != null) {
+            if (invitation.getProposer() != null) {
+                Attendee proposer = new Attendee();
+                proposer.setPlayerId(invitation.getProposer().getPlayerId());
+                proposer.setRole(Role.PROPOSER);
+                attendees.add(proposer);
+            }
+            attendees.addAll(invitation.getGuests().stream().map(p -> {
+                Attendee guest = new Attendee();
+                guest.setPlayerId(p.getPlayerId());
+                guest.setRole(Role.GUEST);
+                return guest;
+            }).collect(Collectors.toList()));
+            
+        }
+        return attendees;
+    }
 }
--- game-engine.core/src/main/java/eu/trentorise/game/model/Invitation.java
+++ game-engine.core/src/main/java/eu/trentorise/game/model/Invitation.java
@@ -1,5 +1,7 @@
 package eu.trentorise.game.model;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import eu.trentorise.game.model.GroupChallenge.PointConceptRef;
@@ -8,8 +10,10 @@ public class Invitation {
 
     private String gameId;
     private Player proposer;
-    private List<Player> guests;
+    private List<Player> guests = new ArrayList<>();
 
+    private Date challengeStart;
+    private Date challengeEnd;
     private PointConceptRef challengePointConcept;
     private Reward reward;
 
@@ -24,6 +28,62 @@ public class Invitation {
             this.playerId = playerId;
         }
 
+    }
+
+    public String getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(String gameId) {
+        this.gameId = gameId;
+    }
+
+    public Player getProposer() {
+        return proposer;
+    }
+
+    public void setProposer(Player proposer) {
+        this.proposer = proposer;
+    }
+
+    public List<Player> getGuests() {
+        return guests;
+    }
+
+    public void setGuests(List<Player> guests) {
+        this.guests = guests;
+    }
+
+    public PointConceptRef getChallengePointConcept() {
+        return challengePointConcept;
+    }
+
+    public void setChallengePointConcept(PointConceptRef challengePointConcept) {
+        this.challengePointConcept = challengePointConcept;
+    }
+
+    public Reward getReward() {
+        return reward;
+    }
+
+    public void setReward(Reward reward) {
+        this.reward = reward;
+    }
+
+    public Date getChallengeStart() {
+        return challengeStart;
+    }
+
+    public void setChallengeStart(Date challengeStart) {
+        this.challengeStart = challengeStart;
+    }
+
+    public Date getChallengeEnd() {
+        return challengeEnd;
+    }
+
+    public void setChallengeEnd(Date challengeEnd) {
+        this.challengeEnd = challengeEnd;
     }
 
 }

b380b3d62cbd787983310600a47a5d586d65de59
mirkoperillo <mirko.perillo@gmail.com>
2018-11-07 15:55:41 +0100

    removed  unused API

--- game-engine.web/src/main/java/eu/trentorise/game/api/rest/ArchiveConceptController.java
+++ game-engine.web/src/main/java/eu/trentorise/game/api/rest/ArchiveConceptController.java
@@ -5,11 +5,8 @@ import java.net.URLDecoder;
 import java.util.Date;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,30 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 import eu.trentorise.game.model.core.ArchivedConcept;
 import eu.trentorise.game.services.PlayerService;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 
 @RestController
 @Profile({ "sec", "no-sec" })
 public class ArchiveConceptController {
 
-	private static final Logger logger = LoggerFactory.getLogger(ArchiveConceptController.class);
-
 	@Autowired
 	private PlayerService playerSrv;
-
-	@RequestMapping(method = RequestMethod.GET, value = "/data/game/archive", produces = { "application/json" })
-	@ApiOperation(value = "Read all archive concepts")
-	public List<ArchivedConcept> readAllArchives(@ApiParam Pageable pageable) {
-
-		List<ArchivedConcept> result = playerSrv.readAllArchiveConcepts(pageable);
-
-		if (logger.isInfoEnabled()) {
-			logger.info(String.format("readAllArchives: %s", result.size()));
-		}
-
-		return result;
-
-	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/data/game/{gameId}/archive", produces = {
 			"application/json" })
@@ -69,10 +49,6 @@ public class ArchiveConceptController {
         }
         List<ArchivedConcept> result =
                 playerSrv.readArchives(gameId, playerId, state, fromDate, toDate);
-
-		if (logger.isInfoEnabled()) {
-			logger.info(String.format("readArchivesForGame: %s", result.size()));
-		}
 
 		return result;
 

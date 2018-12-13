0af3cda49f2278dfcc7b9870276ff10b47dd78a7
Nawaz Khurshid <nawaz1981@gmail.com>
2018-12-13 15:45:36 +0100

    Added wiremock test case.

--- gamification-java-client/src/test/java/it/smartcommunitylab/api/ExampleTest.java
+++ gamification-java-client/src/test/java/it/smartcommunitylab/api/ExampleTest.java
@@ -15,42 +15,32 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 import it.smartcommunitylab.ApiException;
 import it.smartcommunitylab.Configuration;
-import it.smartcommunitylab.auth.HttpBasicAuth;
 import it.smartcommunitylab.model.PlayerStateDTO;
 
 public class ExampleTest {
 
-  private final String userName = "long-rovereto";
-  private final String password = "rov";
-  private static final int PORT = 8089;
-  @Rule public WireMockRule wireMockRule = new WireMockRule(PORT);
+	private static final int PORT = 8089;
+	@Rule
+	public WireMockRule wireMockRule = new WireMockRule(PORT);
 
-  @Test
-  public void exactUrlOnly() throws IOException, ApiException {
-  
-	it.smartcommunitylab.ApiClient client = new it.smartcommunitylab.ApiClient();
-    client.setBasePath("http://localhost:" + PORT);
-    HttpBasicAuth basic = (HttpBasicAuth) client.getAuthentication("basic");
-	basic.setUsername(userName);
-	basic.setPassword(password);
-    Configuration.setDefaultApiClient(client);
+	@Test
+	public void exactUrlOnly() throws IOException, ApiException {
 
-    String gameId = "5b7a885149c95d50c5f9d442";
-    String playerId = "8";
-    
-    String response = "{\"gameId\":\"5b7a885149c95d50c5f9d442\"}";
-    
-    stubFor(
-        get(urlEqualTo("/data/game/" + gameId + "/player/" + playerId))
-            .willReturn(
-                aResponse()
-                    .withHeader("Content-Type", "application/json")
-                    .withBody(response)));
+		it.smartcommunitylab.ApiClient client = new it.smartcommunitylab.ApiClient();
+		client.setBasePath("http://localhost:" + PORT);
+		Configuration.setDefaultApiClient(client);
 
-//    CoreV1Api api = new CoreV1Api();
-    PlayerControllerApi api = new PlayerControllerApi();
-    PlayerStateDTO result = api.readPlayerUsingGET(gameId, playerId);
-   
-    assertEquals(result.getGameId(), gameId);
-  }
+		String gameId = "mockGameId";
+		String playerId = "mockPlayerId";
+
+		String response = "{\"gameId\":\"mockGameId\"}";
+
+		stubFor(get(urlEqualTo("/data/game/" + gameId + "/player/" + playerId))
+				.willReturn(aResponse().withHeader("Content-Type", "application/json").withBody(response)));
+
+		PlayerControllerApi api = new PlayerControllerApi();
+		PlayerStateDTO result = api.readPlayerUsingGET(gameId, playerId);
+
+		assertEquals(result.getGameId(), gameId);
+	}
 }

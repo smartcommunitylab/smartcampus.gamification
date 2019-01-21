13192a4dc24849f4795e7908a91bfd1c62084900
Nawaz Khurshid <nawaz1981@gmail.com>
2019-01-21 10:25:51 +0100

    Fix for update domain issue.

--- game-engine.web/src/main/resources/consoleweb-assets/main.js
+++ game-engine.web/src/main/resources/consoleweb-assets/main.js
@@ -20,17 +20,36 @@ angular.module('gamificationEngine.main', [])
 		$scope.update = function () {
 			$rootScope.domain = $scope.domain;
 			gamesFactory.setUrl('../consoleapi/' + $rootScope.domain);
+			$rootScope.games = [];
+			// Load games
+			gamesFactory.getGames().then(function () {
+				$rootScope.games.forEach(function (g) {
+					g.terminated = g.expiration && g.expiration <= new Date().getTime();
+				});
+			}, function () {
+				// Reject: show error alert
+				$scope.alerts.loadGameError = true;
+			});
 		}
 
 		this.$onInit = function () {
 			gamesFactory.userProfile().then(function (profile) {
-				// profile.domains[1] = "test";
+				//profile.domains[1] = "test";
 				$rootScope.userProfile = profile;
 				$rootScope.domain = profile.domains[0];
 				if ($rootScope.domain != 'ROLE_ADMIN') {
 					gamesFactory.setUrl('../consoleapi/' + $rootScope.domain);
+					// Load games
+					gamesFactory.getGames().then(function () {
+						$rootScope.games.forEach(function (g) {
+							g.terminated = g.expiration && g.expiration <= new Date().getTime();
+						});
+					}, function () {
+						// Reject: show error alert
+						$scope.alerts.loadGameError = true;
+					});
 				}
 			});
-		  }
-		
+		}
+
 	});

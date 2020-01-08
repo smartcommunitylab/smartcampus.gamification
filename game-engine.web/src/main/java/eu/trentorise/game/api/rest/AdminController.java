56f8f097c8c5bc91cee74be6d19ccb3d0b991566
mirkoperillo <mirko.perillo@gmail.com>
2020-01-08 16:22:12 +0100

    admin controller

---
+++ game-engine.web/src/main/java/eu/trentorise/game/api/rest/AdminController.java
@@ -0,0 +1,73 @@
+package eu.trentorise.game.api.rest;
+
+import static eu.trentorise.game.api.rest.ControllerUtils.decodePathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import eu.trentorise.game.bean.PlayerStateDTO;
+import eu.trentorise.game.model.PlayerState;
+import eu.trentorise.game.services.PlayerService;
+import eu.trentorise.game.utils.Converter;
+import springfox.documentation.annotations.ApiIgnore;
+
+@RestController
+@RequestMapping(value = "/admin")
+@Profile({"sec", "no-sec"})
+public class AdminController {
+
+    @Autowired
+    private PlayerService playerSrv;
+    
+    @Autowired
+    private Converter converter;
+
+    @GetMapping(value="/data/game/{gameId}/player")
+    public Page<PlayerStateDTO> readPlayerStates(@PathVariable String gameId,
+            @ApiIgnore Pageable pageable, @RequestParam(required = false) String playerFilter) {
+        gameId = decodePathVariable(gameId);
+        List<PlayerStateDTO> resList = new ArrayList<PlayerStateDTO>();
+        Page<PlayerState> page = null;
+        final boolean hideHiddenChallenges = false;
+        final boolean mergeGroupChallenges = true;
+        if (playerFilter == null) {
+            page = playerSrv.loadStates(gameId, pageable, mergeGroupChallenges,
+                    hideHiddenChallenges);
+        } else {
+            page = playerSrv.loadStates(gameId, playerFilter, pageable, mergeGroupChallenges,
+                    hideHiddenChallenges);
+        }
+        for (PlayerState ps : page) {
+            resList.add(converter.convertPlayerState(ps));
+        }
+
+        PageImpl<PlayerStateDTO> res =
+                new PageImpl<PlayerStateDTO>(resList, pageable, page.getTotalElements());
+
+        return res;
+    }
+
+    @GetMapping(value = "/data/game/{gameId}/player/{playerId}")
+    public PlayerStateDTO readPlayerState(@PathVariable String gameId,
+            @PathVariable String playerId, @ApiIgnore Pageable pageable,
+            @RequestParam(required = false) String playerFilter) {
+        gameId = decodePathVariable(gameId);
+        playerId = decodePathVariable(playerId);
+        final boolean mergeGroupChallenges = true;
+        final boolean hideHiddenChallenges = false;
+        return converter
+                .convertPlayerState(playerSrv.loadState(gameId, playerId, true,
+                        mergeGroupChallenges, hideHiddenChallenges));
+    }
+}

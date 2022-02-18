0d66f03da9ec0e3c8ed46edf1ada9eda5e333450
Nawaz Khurshid <nawaz1981@gmail.com>
2022-02-18 11:35:46 +0100

    fix for test util

---
+++ game-engine.core/src/test/java/eu/trentorise/game/test_utils/Application.java
@@ -0,0 +1,29 @@
+package eu.trentorise.game.test_utils;
+
+import java.util.Date;
+
+public class Application {
+
+	private Date dateApplied;
+
+	private boolean valid;
+
+	public Date getDateApplied() {
+		return dateApplied;
+	}
+
+	public void setDateApplied(Date dateApplied) {
+		this.dateApplied = dateApplied;
+	}
+
+	public boolean isValid() {
+		return valid;
+	}
+
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
+
+	// getter and setter methods here
+
+}

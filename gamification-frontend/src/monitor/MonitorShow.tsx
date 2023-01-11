3a0dbc04e8eb4d0cef20b76bb107662c59278655
Nawaz Khurshid <nawaz1981@gmail.com>
2023-01-11 17:24:51 +0100

    Update MonitorShow.tsx

--- gamification-frontend/src/monitor/MonitorShow.tsx
+++ gamification-frontend/src/monitor/MonitorShow.tsx
@@ -143,7 +143,7 @@ const PeriodField = (props: any) => {
             {
                 Object.entries(record.periods.daily.instances).map((elem:any) => (
                     <li>
-                        <Typography>{elem[1].start} &nbsp;&nbsp; {elem[1].end} &nbsp;&nbsp; {elem[1].score}</Typography>
+                        start: {dateFormatter(elem[1].start)}&nbsp;end: {dateFormatter(elem[1].end)}&nbsp;score: {elem[1].score}
                     </li>
                     
                 ))
@@ -153,3 +153,26 @@ const PeriodField = (props: any) => {
     );
 }
 
+const dateFormatRegex = /^\d{4}-\d{2}-\d{2}$/;
+const dateParseRegex = /(\d{4})-(\d{2})-(\d{2})/;
+
+const dateFormatter = (value:any) => {
+    // null, undefined and empty string values should not go through dateFormatter
+    // otherwise, it returns undefined and will make the input an uncontrolled one.
+    if (value == null || value === '') return '';
+    if (value instanceof Date) return convertDateToString(value);
+    // Valid dates should not be converted
+    if (dateFormatRegex.test(value)) return value;
+
+    return convertDateToString(new Date(value));
+};
+
+const convertDateToString = (value:any) => {
+    // value is a `Date` object
+    if (!(value instanceof Date) || isNaN(value.getDate())) return '';
+    const pad = '00';
+    const yyyy = value.getFullYear().toString();
+    const MM = (value.getMonth() + 1).toString();
+    const dd = value.getDate().toString();
+    return `${yyyy}-${(pad + MM).slice(-2)}-${(pad + dd).slice(-2)}`;
+};

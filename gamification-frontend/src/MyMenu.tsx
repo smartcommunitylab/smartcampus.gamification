6040b5ebce68396180d75be948e91f618999812b
Nawaz Khurshid <nawaz1981@gmail.com>
2023-03-08 11:56:12 +0100

    Update MyMenu.tsx

--- gamification-frontend/src/MyMenu.tsx
+++ gamification-frontend/src/MyMenu.tsx
@@ -1,33 +1,34 @@
 
 import { MenuItemLink, useCreatePath, useGetResourceLabel, useResourceDefinitions, useStore } from 'react-admin';
+import GamesIcon from '@mui/icons-material/Games';
 
 export const MyMenu = () => {
-    
+
     const resources = useResourceDefinitions();
     const getResourceLabel = useGetResourceLabel();
     const createPath = useCreatePath();
     const listHiddenMenu = ['challenges'];
 
     const [gameId, getGameId] = useStore('game.selected');
-    
-    return (Object.keys(resources)
-        .filter(name => name==='game' || ( gameId && !listHiddenMenu.includes(name)))
-        .map(name => (
-            // name==='game'?
-            <MenuItemLink
-                key={name}
-                to={createPath({
-                    resource: name,
-                    type: 'list',
-                })}
-                state={{ _scrollToTop: true }}
-                primaryText={getResourceLabel(name, 2)
-                }
-            />
-            // :null
-        )
-        )
 
-    )
-     
+    return (Object.keys(resources)
+        .filter(name => name === 'game' || (gameId && !listHiddenMenu.includes(name)))
+        .map(name => {
+            if (name == 'game') {
+                return (<MenuItemLink leftIcon={<GamesIcon fontSize='small'/>} key={name} to={createPath({ resource: name, type: 'list', })} state={{ _scrollToTop: true }} primaryText={getResourceLabel(name, 2)} />)
+            }
+            return (<MenuItemLink style={styles.align10} key={name} to={createPath({ resource: name, type: 'list', })} state={{ _scrollToTop: true }} primaryText={getResourceLabel(name, 2)} />)
+        }
+
+        ))
+}
+
+
+const styles = {
+    align10: {
+        left: "10px"
+    },
+    align: {
+        left: '0px'
+    }
 }

7c1b68c786b07a8307d9c81c92bfa636bc10c109
micnori <micnori@users.noreply.github.com>
2023-05-05 11:36:40 +0200

    first impl

---
+++ game-engine.core/src/main/java/eu/trentorise/game/config/RabbitConf.java
@@ -0,0 +1,29 @@
+package eu.trentorise.game.config;
+
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:engine.core.properties")
+public class RabbitConf {
+
+    @Value("${rabbitmq.pngExchangeName}")
+    private String geExchangeName;    
+        
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+    
+    @Bean("gameExchange")
+    DirectExchange gameExchange() {
+        DirectExchange exchange = new DirectExchange(geExchangeName, true, false);
+        return exchange;
+    }
+    
+}
